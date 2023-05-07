5291c2faf0934560dad86f91279cf7450a313c5e
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-05-07 21:42:31 +0200

    Added mock modules to search page
    Added mock modules to search page and styling
    
    Co-Authored-By: Jonel Albuquerque <102956551+u21598267@users.noreply.github.com>

--- Pronto/screens/Timetable/EditTimetable.js
+++ Pronto/screens/Timetable/EditTimetable.js
@@ -6,19 +6,29 @@ const { width } = Dimensions.get('window');
 
 const SearchBar = ({ onSearch }) => {
   const [searchText, setSearchText] = useState('');
+  const [modules, setModules] = useState([
+    { code: 'COS301', name: 'Software Engineering' },
+    { code: 'IMY310', name: 'Multimedia' },
+    { code: 'COS332', name: 'Computer Networking' },
+  ]);
 
   const handleSearch = () => {
     onSearch(searchText);
   };
 
+  const handleDelete = (index) => {
+    const newModules = [...modules];
+    newModules.splice(index, 1);
+    setModules(newModules);
+  };
+
   return (
     <View style={styles.container}>
       <Text style = {styles.heading}>Edit your timetable</Text>
-
       <View style={styles.searchBox}>
         <TextInput
           style={styles.input}
-          placeholder="Search for your module"
+          placeholder="Search for your modules"
           value={searchText}
           onChangeText={setSearchText}
           onSubmitEditing={handleSearch}
@@ -27,6 +37,22 @@ const SearchBar = ({ onSearch }) => {
           <MaterialIcons name="search" size={24} color="black" />
         </TouchableOpacity>
       </View>
+      {modules.map((module, index) => (
+        <TouchableOpacity style={styles.moduleBox} key={module.code}>
+          <View style={styles.moduleInfo}>
+            <Text style={styles.moduleCode}>{module.code}</Text>
+            <Text style={styles.moduleName}>{module.name}</Text>
+          </View>
+          <View style={styles.moduleActions}>
+            <TouchableOpacity onPress={() => handleDelete(index)}>
+              <MaterialIcons name="delete" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <MaterialIcons name="more-vert" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 };
@@ -63,7 +89,33 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
 
-  }
+  },
+  moduleBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginVertical: 5,
+    width: width - 20, // subtracting horizontal padding
+    maxWidth: 600, // limiting to 600 width for tablet layouts
+  },
+  moduleInfo: {},
+  moduleCode: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  moduleName: {
+    fontSize: 14,
+    color: '#555',
+  },
+  moduleActions: 
+  {
+    flexDirection: 'row',
+  },
 });
 
 export default SearchBar;
