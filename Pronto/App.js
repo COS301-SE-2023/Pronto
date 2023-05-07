f2b678584a402d9014c0142727c2858be96e74f9
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-05-07 21:18:49 +0200

    Fixed styling of search page
    Fixed styling of search page, added heading too. Search bar now in the centre and takes up the screen.
    
    Co-Authored-By: Jonel Albuquerque <102956551+u21598267@users.noreply.github.com>

--- Pronto/App.js
+++ Pronto/App.js
@@ -1,24 +1,15 @@
 import {StyleSheet, View,} from 'react-native';
 import Timetable from './screens/Timetable/Timetable';
-<<<<<<< Updated upstream
-import UniversityPage from "./screens/University_Page/University";
-=======
 import EditTimetable from './screens/Timetable/EditTimetable';
->>>>>>> Stashed changes
 
 
 export default function App() {
 
    return (
 
-<<<<<<< Updated upstream
-    <View style={styles.container}>
-       <Timetable/>
-=======
     <View>
      <EditTimetable />
 
->>>>>>> Stashed changes
     </View>
 
   );
--- Pronto/screens/Timetable/EditTimetable.js
+++ Pronto/screens/Timetable/EditTimetable.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, TextInput, StyleSheet, TouchableOpacity, Dimensions, Text } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+
+const { width } = Dimensions.get('window');
 
 const SearchBar = ({ onSearch }) => {
   const [searchText, setSearchText] = useState('');
@@ -11,10 +13,12 @@ const SearchBar = ({ onSearch }) => {
 
   return (
     <View style={styles.container}>
+      <Text style = {styles.heading}>Edit your timetable</Text>
+
       <View style={styles.searchBox}>
         <TextInput
           style={styles.input}
-          placeholder="Search for module"
+          placeholder="Search for your module"
           value={searchText}
           onChangeText={setSearchText}
           onSubmitEditing={handleSearch}
@@ -28,23 +32,38 @@ const SearchBar = ({ onSearch }) => {
 };
 
 const styles = StyleSheet.create({
-  container: 
-  {
+  container: {
     paddingTop: 20,
-    
+    paddingHorizontal: 10,
+    alignItems: 'center',
+   
   },
   searchBox: {
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: 'white',
+    border: "2px solid black",
     borderRadius: 5,
-    paddingHorizontal: 10,
-    border: '2px solid black',
+    width: width - 20, // subtracting horizontal padding
+    maxWidth: 600, // limiting to 600 width for tablet layouts
   },
   input: {
     flex: 1,
     height: 40,
+    textAlign: 'center'
   },
+  heading:
+  {
+    fontSize: 28,
+    position: 'relative',
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+  }
 });
 
 export default SearchBar;
