13b98ad77a6d530b82b802f04fb88b2c693d4a53
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-05-12 20:16:35 +0200

    Added weekly repetition
    Edited schedule timetable. Currently repeats weekly but only if the request has one module.

--- Pronto/screens/Timetable/ScheduleTable.js
+++ Pronto/screens/Timetable/ScheduleTable.js
@@ -2,7 +2,59 @@ import React, {useState} from 'react';
 import {View, TouchableOpacity, Text, Dimensions} from 'react-native';
 import {Agenda} from 'react-native-calendars';
 import {Card} from 'react-native-paper';
-import modules from "../../assets/data/modules.json";
+import modules from "../../assets/data/mock/modules.json";
+
+//function to take in a day, and give all dates of the year that a day occurs
+function getDatesForDayOfWeek(dayOfWeek) {
+  const date = new Date();
+  const year = date.getFullYear();
+  const dayIndex = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'Friday', 'saturday'].indexOf(dayOfWeek);
+  const results = [];
+
+  // Loop through each month of the year
+  for (let month = 0; month < 12; month++) {
+    // Create a new date object for the first day of the month
+    const firstDayOfMonth = new Date(year, month, 1);
+
+    // Find the first occurrence of the specified day of the week
+    const diff = dayIndex - firstDayOfMonth.getDay();
+    let dayOfMonth = diff >= 0 ? diff + 1 : diff + 8;
+
+    // Loop through the rest of the month, adding dates for the specified day of the week
+    while (dayOfMonth <= new Date(year, month + 1, 0).getDate()) {
+      const dateString = `${year}-${(month + 1).toString().padStart(2, '0')}-${dayOfMonth.toString().padStart(2, '0')}`;
+      results.push(dateString);
+      dayOfMonth += 7;
+    }
+  }
+
+  return results;
+}
+
+
+function createScheduleArray(module) 
+{
+  const dates = getDatesForDayOfWeek(module.day);
+
+  const scheduleArray = {};
+
+  dates.forEach(date => {
+    scheduleArray[date] = [{
+      id: module.id,
+      code: module.code,
+      time: module.time,
+      frequency: module.frequency,
+      venue: module.venue,
+      day: module.day,
+      height: 50,
+    }];
+  });
+
+  return scheduleArray;
+}
+
+var scheduleArray = createScheduleArray(modules);
+console.log(scheduleArray);
 
 const ScheduleTable = () => 
 {
@@ -27,17 +79,18 @@ const ScheduleTable = () =>
       </TouchableOpacity>
     );
   };
-
   const windowHeight = Dimensions.get('window').height;
   const windowWidth = Dimensions.get('window').width;
   var date = new Date().getDate();
   var month = new Date().getMonth() + 1;
   var year = new Date().getFullYear();
+ 
 
+  
   return (
     <View style={{height: windowHeight, width: windowWidth}}>
       <Agenda
-        items={modules}
+        items={scheduleArray}
         selected={year + '-' + month + '-' + date}
         renderItem={renderItem}
       />
