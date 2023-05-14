1d5d2781f67283eaaffb2bb1355da74ae578b744
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-05-14 08:45:18 +0200

    uni names with domains and countyr

--- data/cleanUniversityData.py
+++ data/cleanUniversityData.py
@@ -3,19 +3,26 @@ import json
 filePaths = {
 	'mainSource': 'universitiesNamesAndDomainsData.json',
 	'names':'universityNames.json',
-	'domains':'universityDomains.json'
-	''
+	'domains':'universityDomains.json',
+	'namesWithDomains': 'universityNamesWithDomains.json'
 }
 
 universityData:dict
 univerityNames = []
 universityDomains = []
+universityNamesWithDomains = []
 with open(filePaths['mainSource'], 'r') as json_file:
 	universityData = json.load(json_file)
 
-for universityInfo in universityData:
+# Extract Important data
+for universityInfo in universityData:   
+	del universityInfo['web_pages']
+	del universityInfo['alpha_two_code']
+	if 'state-province' in universityInfo:
+		del universityInfo['state-province']
 	univerityNames.append(universityInfo['name'])
 	universityDomains.extend(universityInfo['domains'])
+	universityNamesWithDomains.append(universityInfo)
 	
 # Output as JSON
 with open(filePaths['names'], "w") as outfile:
@@ -23,3 +30,6 @@ with open(filePaths['names'], "w") as outfile:
 
 with open(filePaths['domains'], "w") as outfile:
     outfile.write(json.dumps(universityDomains,ensure_ascii=False,indent=4))
+
+with open(filePaths['namesWithDomains'], "w") as outfile:
+    outfile.write(json.dumps(universityNamesWithDomains,ensure_ascii=False,indent=4))
