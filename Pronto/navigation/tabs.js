24b6a88e4f8ca1b894bd9c0786b5072d0264a8c7
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-05-15 23:41:36 +0200

    Added some styling to navigation
    Added some styling to navigation

--- Pronto/navigation/tabs.js
+++ Pronto/navigation/tabs.js
@@ -4,12 +4,31 @@ import EditTimetable from "../screens/Timetable/EditTimetable";
 import AccountSettings from "../screens/Timetable/AccountSettings";
 import Notifications from "../screens/Timetable/Notifications";
 import NavigationScreen from "../screens/Timetable/NavigationScreen";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
+const screenOptions = {
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 25,
+    left: 20,
+    right: 20,
+    elevation: 0,
+    backgroundColor: "white",
+    borderRadius: 15,
+    height: 90,
+  },
+};
+
 const Tabs = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+      }}
+      {...{ screenOptions }}
+    >
       <Tab.Screen name="Schedule" component={ScheduleTable} />
       <Tab.Screen name="Navigation" component={NavigationScreen} />
       <Tab.Screen name="Search" component={EditTimetable} />
