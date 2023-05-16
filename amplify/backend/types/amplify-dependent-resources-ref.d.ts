3fda91c58bb6c6bfa0cfe1f8f4a22c4abe5ff349
Bandisa Masilela <89698022+Bandisa@users.noreply.github.com>
2023-05-16 17:54:36 +0200

    initial project set-up, with dynamoDB,S3, Auth and GraphQL API (#15)
    * initial project set-up, with dynamoDB,S3, Auth and GraphQL API
    
    * removing env

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "pronto": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "pronto": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3prontoblobestore": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
