21fce28a25a6c05b0c8294b8f7d7d35a68b98857
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-05-18 00:08:19 +0200

    Added map to navigation page
    Added map to navigation page

--- Pronto/Testing/Notifications/NotificationList.test.js
+++ Pronto/Testing/Notifications/NotificationList.test.js
@@ -1,44 +1,49 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
-import NotificationList from '../../components/NotificationList';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import NotificationList from "../../components/NotificationList";
 
-describe('NotificationList', () => {
+describe("NotificationList", () => {
+  //test that all the titles are rendered
+  test("renders correctly", () => {
+    const { getByText } = render(<NotificationList />);
+    expect(getByText("Announcements")).toBeTruthy();
+    expect(getByText("Reminders")).toBeTruthy();
+    expect(getByText("Due Dates")).toBeTruthy();
+    expect(getByText("Unread")).toBeTruthy();
+  });
 
-    //test that all the titles are rendered
-    test('renders correctly', () => {
-        const { getByText } = render(<NotificationList />);
-        expect(getByText('Announcements')).toBeTruthy();
-        expect(getByText('Reminders')).toBeTruthy();
-        expect(getByText('Due Dates')).toBeTruthy();
-        expect(getByText('Other')).toBeTruthy();
-    });
+  //test that the cards are rendered and that they are clickable
+  test("expands and collapses Reminders accordion", () => {
+    const { getByText } = render(<NotificationList />);
+    fireEvent.press(getByText("Reminders")); //press the reminders accordion
+    expect(
+      getByText("COS 301: Lecture venue changed from North Hall to IT 2-27")
+    ).toBeTruthy();
+    fireEvent.press(getByText("Reminders"));
+    expect(() =>
+      getByText("COS 301: Lecture venue changed from North Hall to IT 2-27")
+    ).toThrow();
+  });
 
-    //test that the cards are rendered and that they are clickable
-    test('expands and collapses Reminders accordion', () => {
-        const { getByText } = render(<NotificationList />);
-        fireEvent.press(getByText('Reminders')); //press the reminders accordion
-        expect(getByText('COS 301: Lecture venue changed from North Hall to IT 2-27')).toBeTruthy();
-        fireEvent.press(getByText('Reminders'));
-        expect(() => getByText('COS 301: Lecture venue changed from North Hall to IT 2-27')).toThrow();
-    });
+  //test that the cards are rendered and that they are clickable
+  test("expands and collapses Due Dates accordion", () => {
+    const { getByText } = render(<NotificationList />);
+    fireEvent.press(getByText("Due Dates"));
+    expect(getByText("COS216: Assignment due soon")).toBeTruthy();
+    fireEvent.press(getByText("Due Dates")); // press the accordion again to collapse it
+    expect(() => getByText("COS216: Assignment due soon")).toThrow();
+  });
 
-    //test that the cards are rendered and that they are clickable
-    test('expands and collapses Due Dates accordion', () => {
-        const { getByText } = render(<NotificationList />);
-        fireEvent.press(getByText('Due Dates'));
-        expect(getByText('COS216: Assignment due soon')).toBeTruthy();
-        fireEvent.press(getByText('Due Dates')); // press the accordion again to collapse it
-        expect(() => getByText('COS216: Assignment due soon')).toThrow();
-    });
-
-    //test that the cards are rendered and that they are clickable
-    test('expands and collapses Other accordion', () => {
-        const { getByText } = render(<NotificationList />);
-        fireEvent.press(getByText('Other'));
-        expect(getByText('IMY310: Remeber your pens for the semester test')).toBeTruthy();
-        fireEvent.press(getByText('Other'));
-        expect(() => getByText('IMY310: Remeber your pens for the semester test')).toThrow();
-    });
-
-
+  //test that the cards are rendered and that they are clickable
+  test("expands and collapses Other accordion", () => {
+    const { getByText } = render(<NotificationList />);
+    fireEvent.press(getByText("Unread"));
+    expect(
+      getByText("IMY310: Remeber your pens for the semester test")
+    ).toBeTruthy();
+    fireEvent.press(getByText("Unread"));
+    expect(() =>
+      getByText("IMY310: Remeber your pens for the semester test")
+    ).toThrow();
+  });
 });
