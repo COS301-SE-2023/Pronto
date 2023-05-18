bee889b9e9a4e1736cdce550a88935279e8508f9
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-05-18 18:18:23 +0200

    Removed unnecessary screens
    Removed unnecessary screens

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
-import HomeScreen from "./screens/HomeScreen";
-import EditTimetable from "./screens/Timetable/EditTimetable";
-import ScheduleTable from "./screens/Timetable/ScheduleTable";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/tabs";
 
-export default function App() 
-{
+export default function App() {
   return (
     <NavigationContainer>
       <Tabs />
