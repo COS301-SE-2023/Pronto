5286b3665e24c01105cf34d8a03219d6b1aeec25
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-05-21 19:23:16 +0200

    fixing merge conf

---
+++ App.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/tabs";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
+}
