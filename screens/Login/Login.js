471f95334d209742c03767e6e2b1978b7b2e0c50
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-05-27 12:14:07 +0200

    Created sign up page + centered contents on sign up and login page
    Created sign up page + centered contents on sign up and login page

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -15,140 +15,117 @@ const { height } = Dimensions.get("window");
 
 const Login = ({ navigation }) => {
   return (
-    <SafeAreaView style={{}}>
-      <View
-        style={{
-          padding: 24,
-        }}
-      >
-        <View
-          style={{
-            alignItems: "center",
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 40,
-              color: "#e32f45",
-              fontWeight: "bold",
-              marginTop: 20,
-              opacity: 0.9,
-            }}
-          >
-            Login here
-          </Text>
-
-          <ImageBackground
-            style={{
-              height: height / 5,
-              width: "100%",
-              alignSelf: "center",
-            }}
-            resizeMode="contain"
-            source={require("../../assets/icons/ProntoLogo.png")}
-          />
-
-          <Text
-            style={{
-              fontWeight: "bold",
-              textAlign: "center",
-              fontSize: 15,
-            }}
-          >
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <View style={styles.centered}>
+          <Text style={styles.title}>Login here</Text>
+          <Text style={styles.subtitle}>
             Welcome back, sign in to access all your features!
           </Text>
         </View>
 
-        <View
-          style={{
-            marginVertical: 40,
-          }}
-        >
+        <View style={styles.inputContainer}>
           <TextInput
             placeholder="Email"
             placeholderTextColor={"#666666"}
-            style={{
-              fontSize: 20,
-              padding: 20,
-              borderRadius: 10,
-              backgroundColor: "#d1d1d1",
-              opacity: 0.5,
-              marginVertical: 10,
-            }}
+            style={styles.input}
           />
 
           <TextInput
             placeholder="Password"
             placeholderTextColor={"#666666"}
             secureTextEntry={true}
-            style={{
-              fontSize: 20,
-              padding: 20,
-              borderRadius: 10,
-              backgroundColor: "#d1d1d1",
-              opacity: 0.5,
-              marginVertical: 10,
-            }}
+            style={styles.input}
           />
         </View>
 
         <View>
-          <Text
-            style={{
-              fontSize: 15,
-              fontWeight: "bold",
-              color: "#e32f45",
-              alignSelf: "flex-end",
-            }}
-          >
-            Forgot your password?
-          </Text>
+          <Text style={styles.forgotPassword}>Forgot your password?</Text>
         </View>
 
-        <TouchableOpacity
-          style={{
-            padding: 20,
-            backgroundColor: "#e32f45",
-            marginVertical: 20,
-            borderRadius: 10,
-            shadowColor: "#e32f45",
-            shadowOpacity: 0.3,
-            shadowOffset: { width: 0, height: 10 },
-            shadowRadius: 10,
-          }}
-        >
-          <Text
-            style={{
-              color: "#fff",
-              textAlign: "center",
-              fontSize: 20,
-              fontWeight: "bold",
-            }}
-          >
-            Sign in
-          </Text>
+        <TouchableOpacity style={styles.signInButton}>
+          <Text style={styles.signInButtonText}>Sign in</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          style={{
-            padding: 10,
-          }}
+          style={styles.createAccountButton}
           onPress={() => navigation.navigate("Register")}
         >
-          <Text
-            style={{
-              color: "black",
-              textAlign: "center",
-              fontSize: 15,
-              fontWeight: "bold",
-            }}
-          >
-            Create new account
-          </Text>
+          <Text style={styles.createAccountButtonText}>Create new account</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+  centered: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 33,
+    color: "#e32f45",
+    fontWeight: "bold",
+    marginTop: 20,
+    opacity: 0.9,
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 15,
+    maxWidth: "70%",
+  },
+  inputContainer: {
+    marginVertical: 40,
+  },
+  input: {
+    fontSize: 15,
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: "#E7DADA",
+    opacity: 0.7,
+    marginVertical: 10,
+  },
+  forgotPassword: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#e32f45",
+    alignSelf: "flex-end",
+  },
+  signInButton: {
+    padding: 20,
+    backgroundColor: "#e32f45",
+    marginVertical: 20,
+    borderRadius: 10,
+    shadowColor: "#e32f45",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 10 },
+    shadowRadius: 10,
+  },
+  signInButtonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  createAccountButton: {
+    padding: 10,
+  },
+  createAccountButtonText: {
+    color: "black",
+    textAlign: "center",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+});
 
 export default Login;
