06b3b97f6c5bcd70a5979fe4f0a9077b594e633c
u21598267- J. Albuquerque <u21598267@tuks.co.za>
2023-05-31 11:09:08 +0200

    Navigation added
    ##Navigation added
    - Routing for the components have been added
    - Routing added for Lecture/Admin home pages and login pages
    
    Co-Authored-By: Tyrone Sutherland-MacLeod <105007195+Tymac15@users.noreply.github.com>
    Co-Authored-By: ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>

--- lecturer web view/lecturer-view/src/App.js
+++ lecturer web view/lecturer-view/src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
 import PostReminder from "./LectureView/PostReminder";
-//import InstitutionHomePage from "./Institution VIew/InstitutionHomePage";
-//import LectureHomePage from "./LectureView/LectureHomePage";
-//import Login from "./Authentication/LecturerLogin/Login";
+import LectureHomePage from "./LectureView/LectureHomePage";
+import Login from "./Authentication/LecturerLogin/Login";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import InstitutionHomePage from "./Institution VIew/InstitutionHomePage";
+
+
 
 const Home = () => {
   return (
-    <div>
-      <PostReminder />
-      {/*<Login />*/}
-      {/*<LectureHomePage />*/}
-      {/* <InstitutionHomePage />*/}
-    </div>
+
+        <div>
+            <Router >
+                    <Routes>
+                        <Route path="/" element={<InstitutionHomePage />} classname={"mt-5"}/>
+                        <Route path="/login" element={<Login/>} />
+                        <Route path="/lecture-homepage" element={<LectureHomePage />} />
+                    </Routes>
+            </Router>
+
+        </div>
+
+
   );
 };
 
