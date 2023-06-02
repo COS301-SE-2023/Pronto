b5cee7d79e80d056c77c84cb3e42574ab163c8e8
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-06-02 20:18:10 +0200

    Added autologin
    Added auto login when user signs in recently

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import Login from "./screens/Login/Login";
 import ResetPassword from "./screens/Login/ResetPassword";
 import VerifyCode from "./screens/Login/VerifyCode";
 import ConfirmEmail from "./screens/Login/ConfirmEmail";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, View, Alert } from "react-native";
 
 import { Amplify } from "aws-amplify";
 import { Auth } from "aws-amplify";
@@ -19,64 +21,91 @@ Auth.configure(config);
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [user, setUser] = useState(undefined);
+
+  const checkUser = async () => {
+    try {
+      const authUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      setUser(authUser);
+    } catch (e) {
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  if (user == undefined) {
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ActivityIndicator />
+    </View>;
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Welcome"
-          component={WelcomeScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
+      {user ? (
+        <Tabs />
+      ) : (
+        <>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Welcome"
+              component={WelcomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
 
-        <Stack.Screen
-          name="Register"
-          component={Register}
-          options={{
-            headerShown: false,
-          }}
-        />
+            <Stack.Screen
+              name="Register"
+              component={Register}
+              options={{
+                headerShown: false,
+              }}
+            />
 
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            headerShown: false,
-          }}
-        />
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{
+                headerShown: false,
+              }}
+            />
 
-        <Stack.Screen
-          name="ResetPassword"
-          component={ResetPassword}
-          options={{
-            headerShown: false,
-          }}
-        />
+            <Stack.Screen
+              name="ResetPassword"
+              component={ResetPassword}
+              options={{
+                headerShown: false,
+              }}
+            />
 
-        <Stack.Screen
-          name="VerifyCode"
-          component={VerifyCode}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="ConfirmEmail"
-          component={ConfirmEmail}
-          options={{
-            headerShown: false,
-          }}
-        />
-
-        <Stack.Screen
-          name="NotificationPreferences"
-          component={NotificationPreferences}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
+            <Stack.Screen
+              name="VerifyCode"
+              component={VerifyCode}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="ConfirmEmail"
+              component={ConfirmEmail}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </>
+      )}
     </NavigationContainer>
 
     /* <NavigationContainer>
