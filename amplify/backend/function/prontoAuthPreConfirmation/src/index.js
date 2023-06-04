8222c1947592b3dff782f1ee44b0724e23463826
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-06-04 21:17:31 +0200

    check admin

--- amplify/backend/function/prontoAuthPreConfirmation/src/index.js
+++ amplify/backend/function/prontoAuthPreConfirmation/src/index.js
@@ -10,28 +10,47 @@
 	AdminGroupName
 Amplify Params - DO NOT EDIT */
 const { ROLES } = require('../../prontoAuthPostConfirmation/src/roles');
-const isEmailPartOfInstitution = require('./confirmLectureEmail');
+const { isLectureEmailPartOfInstitution,
+        isAdminAllocated
+} = require('./confirmEmails');
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
     if(!event.request.clientMetadata.role)
         throw new Error('User role not provided on ClientMetadata')
-    switch (event.request.clientMetadata.role) {
-        case ROLES.Admin:
-            
-            break;
-        case ROLES.Lecture:
-            
-            break;
-        case ROLES.Student:
-            //call api
-            //get email domains for the institution
-            //check student domain
-            //event.response.autoConfirmUser = isStudentDomainPartOfInstitution();
-            break;
-        default:
-            break;
+    event.response.autoConfirmUser = false;
+    try {        
+        switch (event.request.clientMetadata.role) {
+            case ROLES.Admin:
+                if(isAdminAllocated(event.request.clientMetadata.institutionId)){
+                    event.response.autoConfirmUser = false;
+                    throw new Error('Institution has an admin already');
+                }
+                event.response.autoConfirmUser = true;
+                break;
+            case ROLES.Lecture:
+                const isLectureEmailPartOfInstitution = isLectureEmailPartOfInstitution(
+                    event.request.userAttributes.email,
+                    event.request.clientMetadata.institutionId
+                );
+                if(!isLectureEmailPartOfInstitution){
+                    
+                }
+                event.response.autoConfirmUser = true;
+                break;
+            case ROLES.Student:
+                //call api
+                //get email domains for the institution
+                //check student domain
+                //event.response.autoConfirmUser = isStudentDomainPartOfInstitution();
+                break;
+            default:
+                break;
+        }
+    } catch (error) {
+        
     }
     return event;
 };
