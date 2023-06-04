519274c6916c40a063f557a261c542647ce52f1d
u21598267- J. Albuquerque <u21598267@tuks.co.za>
2023-06-04 15:09:59 +0200

    Testing Added
    ## Testing has been completed for:
    - Navigation
    - File Upload (Drag and Drop)
    ## To do:
    - Add lecturer
    - File Upload
    - Home Page
    - Log in
    - Respective Lecturer view pages

--- Web view/Web app/src/Institution VIew/FileUpload/DragandDrop.js
+++ Web view/Web app/src/Institution VIew/FileUpload/DragandDrop.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import CloseIcon from '@mui/icons-material/Close';
-import {IconButton} from "@mui/material";
+import { IconButton } from '@mui/material';
 
 const baseStyle = {
     display: 'flex',
@@ -35,13 +35,15 @@ function DropzoneComponent(props) {
     const onDrop = useCallback(acceptedFiles => {
         setFiles(prevFiles => [
             ...prevFiles,
-            ...acceptedFiles.map(file => Object.assign(file, {
-                preview: URL.createObjectURL(file)
-            }))
+            ...acceptedFiles.map(file =>
+                Object.assign(file, {
+                    preview: URL.createObjectURL(file)
+                })
+            )
         ]);
     }, []);
 
-    const removeFile = (file) => {
+    const removeFile = file => {
         setFiles(prevFiles => prevFiles.filter(f => f !== file));
         URL.revokeObjectURL(file.preview);
     };
@@ -57,38 +59,34 @@ function DropzoneComponent(props) {
         accept: 'application/pdf'
     });
 
-    const style = useMemo(() => ({
-        ...baseStyle,
-        ...(isDragActive ? activeStyle : {}),
-        ...(isDragAccept ? acceptStyle : {}),
-        ...(isDragReject ? rejectStyle : {})
-    }), [
-        isDragActive,
-        isDragReject,
-        isDragAccept
-    ]);
+    const style = useMemo(
+        () => ({
+            ...baseStyle,
+            ...(isDragActive ? activeStyle : {}),
+            ...(isDragAccept ? acceptStyle : {}),
+            ...(isDragReject ? rejectStyle : {})
+        }),
+        [isDragActive, isDragReject, isDragAccept]
+    );
 
     const thumbs = files.map(file => (
         <div key={file.name}>
             {file.type.includes('image') ? (
                 <div>
-                    <img
-                        src={file.preview}
-                        alt={file.name}
-                    />
-                    <button className="btn btn-danger" onClick={() => removeFile(file)}>
+                    <img src={file.preview} alt={file.name} data-testid="image-preview" />
+                    <button className="btn btn-danger" onClick={() => removeFile(file)} data-testid="remove-button">
                         Remove
                     </button>
                 </div>
             ) : (
                 <div>
-                    <span>{file.name}</span>
+                    <span data-testid={'pdfName'}>{file.name}</span>
                     <br />
-                    <a href={file.preview} target="_blank" rel="noopener noreferrer">
+                    <a href={file.preview} target="_blank" rel="noopener noreferrer" data-testid={"pdf-link"}>
                         View PDF
                     </a>
-                    <IconButton onClick={() => removeFile(file)} name={'Remove'}>
-                        <CloseIcon/>
+                    <IconButton onClick={() => removeFile(file)} name={'Remove'}  data-testid={'removePdf'}>
+                        <CloseIcon />
                     </IconButton>
                 </div>
             )}
@@ -100,16 +98,14 @@ function DropzoneComponent(props) {
     }, [files]);
 
     return (
-        <div className="card">
+        <div className="card" data-testid="dropzone">
             <div className="card-body">
                 <section>
                     <div {...getRootProps({ style })}>
-                        <input {...getInputProps()} />
+                        <input {...getInputProps()} data-testid="drop-input" />
                         <div>Click here or Drag and drop your images or PDFs here.</div>
                     </div>
-                    <aside>
-                        {thumbs}
-                    </aside>
+                    <aside data-testid="thumbs-container">{thumbs}</aside>
                 </section>
             </div>
         </div>
