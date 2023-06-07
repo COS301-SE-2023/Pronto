3cfbf5c847beb0aec4c74d82f399a75d1232964e
Tymac15 <105007195+Tymac15@users.noreply.github.com>
2023-06-07 10:07:08 +0200

    Update RecentAnnouncement.test.js

--- Web view/Web app/src/__testing__/RecentAnnouncement.test.js
+++ Web view/Web app/src/__testing__/RecentAnnouncement.test.js
@@ -27,37 +27,4 @@ describe('RecentAnnouncement', () => {
         expect(card2).toBeInTheDocument();
     });
 
-
-
-    //test the dropdowns
-    it('Test if drop down menu 1 is rendered', () => {
-        const dropDown = screen.getByTestId('paccordionDrop');
-        expect(dropDown).toBeInTheDocument();
-
-        //check that the dropdown menu is clickable
-        fireEvent.click(dropDown);
-    });
-
-
-
-    //test form1 submission by filling in the form and clicking the submit button
-    it('Test if form 2 is submitted', () => {
-        const pword = screen.getByTestId('pword');
-        const repword = screen.getByTestId('repword');
-        const newpword = screen.getByTestId('newpword');
-
-
-        //fill in the form
-        fireEvent.change(pword, { target: { value: 'test' } });
-        fireEvent.change(repword, { target: { value: 'test' } });
-        fireEvent.change(newpword, { target: { value: '09 Jun 2023' } });
-
-        //check that the form is filled in
-        expect(pword.value).toBe('test');
-        expect(repword.value).toBe('test');
-        expect(newpword.value).toBe('09 Jun 2023');
-
-    });
-
-
 });
