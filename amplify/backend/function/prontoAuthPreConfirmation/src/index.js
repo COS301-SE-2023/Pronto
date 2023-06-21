aba4940d8ec41c9de84905083e4d59e1ccc28dad
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-06-21 04:48:09 +0200

    confirming app client for each user type

--- amplify/backend/function/prontoAuthPreConfirmation/src/index.js
+++ amplify/backend/function/prontoAuthPreConfirmation/src/index.js
@@ -10,49 +10,55 @@
 	AdminGroupName
 Amplify Params - DO NOT EDIT */
 const ROLES = require('./roles.js');
-const { isLectureEmailPartOfInstitution,
-        isAdminAllocated
-    } = require('./confirmEmails.js');
-
+const { isLectureEmailPartOfInstitution, isAdminAllocated } = require('./confirmEmails.js');
+const isAppClientValid = require(',/isAppClientValid.js');
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    if(!event.request.clientMetadata.role)
-        throw new Error('User role not provided on ClientMetadata')
-    event.response.autoConfirmUser = false;
-    try {        
-        switch (event.request.clientMetadata.role) {
-            case ROLES.Admin:
-                if(isAdminAllocated(event.request.clientMetadata.institutionId)){
-                    event.response.autoConfirmUser = false;
-                    throw new Error(`Institution has an admin already. institutionId=${event.request.clientMetadata.institutionId}`);
-                }
-                event.response.autoConfirmUser = true;
-                break;
-            case ROLES.Lecture:
-                const isLectureEmailPartOfInstitution = isLectureEmailPartOfInstitution(
-                    event.request.userAttributes.email,
-                    event.request.clientMetadata.institutionId
-                );
-                if(isLectureEmailPartOfInstitution){
-                    event.response.autoConfirmUser = true;
-                }
-                event.response.autoConfirmUser = false;
-                throw new Error(`Lecturer email is not part of the Institution. institutionId=${event.request.clientMetadata.institutionId}`);
-            case ROLES.Student:
-                //call api
-                //get email domains for the institution
-                //check student domain
-                //event.response.autoConfirmUser = isStudentDomainPartOfInstitution();
-                event.response.autoConfirmUser = false;
-                break;
-            default:
-                break;
+  if (!event.request.clientMetadata.role) throw new Error('User role not provided on ClientMetadata');
+  if (!isAppClientValid(event.request.clientMetadata.role))
+    throw new Error(
+      `Cannot authenticate users from this app client: Students Should use the mobile app and Admin/Lectures should use the web app`,
+    );
+    
+  event.response.autoConfirmUser = false;
+  try {
+    switch (event.request.clientMetadata.role) {
+      case ROLES.Admin:
+        if (await isAdminAllocated(event.request.clientMetadata.institutionId)) {
+          event.response.autoConfirmUser = false;
+          throw new Error(
+            `Institution has an admin already. institutionId=${event.request.clientMetadata.institutionId}`,
+          );
         }
-    } catch (apiHandlerError) {
-        //db error, add error handling mechanisms
-        throw new Error(apiHandlerError); //send error to client
+        event.response.autoConfirmUser = true;
+        break;
+      case ROLES.Lecture:
+        const isLectureEmailPartOfInstitution = isLectureEmailPartOfInstitution(
+          event.request.userAttributes.email,
+          event.request.clientMetadata.institutionId,
+        );
+        if (isLectureEmailPartOfInstitution) {
+          event.response.autoConfirmUser = true;
+        }
+        event.response.autoConfirmUser = false;
+        throw new Error(
+          `Lecturer email is not part of the Institution. institutionId=${event.request.clientMetadata.institutionId}`,
+        );
+      case ROLES.Student:
+        //call api
+        //get email domains for the institution
+        //check student domain
+        //event.response.autoConfirmUser = isStudentDomainPartOfInstitution();
+        event.response.autoConfirmUser = false;
+        break;
+      default:
+        break;
     }
-    return event;
+  } catch (apiHandlerError) {
+    //db error, add error handling mechanisms
+    throw new Error(apiHandlerError); //send error to client
+  }
+  return event;
 };
