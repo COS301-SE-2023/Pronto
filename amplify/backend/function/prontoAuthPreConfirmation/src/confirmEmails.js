5db7e8ceef5c61a35519285b170cd06a71746f7c
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-06-21 02:31:03 +0200

    error logging: getEmailsQueryError

--- amplify/backend/function/prontoAuthPreConfirmation/src/confirmEmails.js
+++ amplify/backend/function/prontoAuthPreConfirmation/src/confirmEmails.js
@@ -1,7 +1,7 @@
 const GRAPHQL_ENDPOINT = process.env.API_API_PRONTO_GRAPHQLAPIENDPOINTOUTPUT;
 const GRAPHQL_API_KEY = process.env.API_API_PRONTO_GRAPHQLAPIKEYOUTPUT;
 let institutionDetails;
-const getAndSetInstitutionDetails = async(institutionId) => {
+const getAndSetInstitutionDetails = async (institutionId) => {
   const query = /* GraphQL */ `
       query MyQuery($id: ID = ${institutionId}) {
         getInstitution(id: $id) {
@@ -11,76 +11,70 @@ const getAndSetInstitutionDetails = async(institutionId) => {
         }
       }
     `;
-  
+
   const options = {
     method: 'POST',
     headers: {
       'x-api-key': GRAPHQL_API_KEY,
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ query })
+    body: JSON.stringify({ query }),
   };
-  
+
   const request = new Request(GRAPHQL_ENDPOINT, options);
   let body;
   let response;
-  
+
   try {
     response = await fetch(request);
     body = await response.json();
     if (body.errors)
-        return {
-            error: `Failed To retrieve institution details. id=${institutionId}.
-             Info: ${body.errors}`
-        };
-    return institutionDetails = body.data.getInstitution;
-  } catch (getEmailsQueryError) {
-    return {
+      return {
         error: `Failed To retrieve institution details. id=${institutionId}.
-         Info: ${getEmailsQueryError}`
-    };
+             Info: ${body.errors}`,
+      };
+    return (institutionDetails = body.data.getInstitution);
+  } catch (getEmailsQueryError) {
+    console.debug(getEmailsQueryError);
+    throw new Error(`Failed To retrieve institution details. id=${institutionId}.`);
   }
-}
+};
 
 const getLectureEmailsFromInstitution = async (institutionId) => {
-  if(!institutionDetails){
+  if (!institutionDetails) {
     const results = await getAndSetInstitutionDetails(institutionId);
-    if(!results.error)
-      return institutionDetails.lectureremails;
+    if (!results.error) return institutionDetails.lectureremails;
     throw new Error(results);
   }
   return institutionDetails.lectureremails;
-}
+};
 
 const getInstitutionAdminId = async (institutionId) => {
-  if(!institutionDetails){
+  if (!institutionDetails) {
     const results = await getAndSetInstitutionDetails(institutionId);
-    if(!results.error)
-      return institutionDetails.adminId;
+    if (!results.error) return institutionDetails.adminId;
     throw new Error(results);
   }
   return institutionDetails.adminId;
-}
-    
-const isLectureEmailPartOfInstitution = async(email, institutionId) =>{
+};
+
+const isLectureEmailPartOfInstitution = async (email, institutionId) => {
   try {
     const emailList = await getLectureEmailsFromInstitution(institutionId);
     return emailList.includes(email);
   } catch (getLectureEmailsFromInstitutionError) {
-    throw new Error(getLectureEmailsFromInstitutionError)
+    throw new Error(getLectureEmailsFromInstitutionError);
   }
-}
+};
 
-const isAdminAllocated = function(institutionId) {
+const isAdminAllocated = function (institutionId) {
   try {
     const adminId = getInstitutionAdminId(institutionId);
     return adminId != null;
-  } catch (error) {
-    
-  }
-}
+  } catch (error) {}
+};
 
-module.exports ={
-    isLectureEmailPartOfInstitution,
-    isAdminAllocated
-}
+module.exports = {
+  isLectureEmailPartOfInstitution,
+  isAdminAllocated,
+};
