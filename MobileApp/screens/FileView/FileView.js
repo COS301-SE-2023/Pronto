68b31dc65b0e63e0e533fd6fa0a23de5704955c0
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-06-25 18:46:00 +0200

    Added styling to file view
    Added styling to file view

--- MobileApp/screens/FileView/FileView.js
+++ MobileApp/screens/FileView/FileView.js
@@ -6,8 +6,11 @@ import {
   TouchableOpacity,
   Linking,
   Alert,
+  StyleSheet,
 } from "react-native";
 import { Storage } from "aws-amplify";
+
+let studentUniversity = "UniversityOfPretoria";
 
 const BucketFilesScreen = () => {
   const [fileList, setFileList] = useState([]);
@@ -18,14 +21,13 @@ const BucketFilesScreen = () => {
 
   const fetchFileList = async () => {
     try {
-      const response = await Storage.list("UniversityOfPretoria/", {
+      const response = await Storage.list(studentUniversity + "/", {
         pageSize: 1000,
       });
       const files = response.results;
       setFileList(files);
-      Alert.alert(JSON.stringify(files));
     } catch (error) {
-      console.error("Error fetching file list:", error);
+      Alert.alert("Error fetching file list:", error);
     }
   };
 
@@ -39,17 +41,20 @@ const BucketFilesScreen = () => {
   };
 
   const renderFileItem = ({ item }) => {
-    const fileName = item.key.replace("UniversityOfPretoria/", ""); // Extract file name
+    const fileName = item.key.replace(studentUniversity + "/", ""); // Extract file name
     return (
-      <TouchableOpacity onPress={() => openFile(item.key)}>
-        <Text>{fileName}</Text>
+      <TouchableOpacity
+        onPress={() => openFile(item.key)}
+        style={styles.fileItem}
+      >
+        <Text style={styles.fileName}>{fileName}</Text>
       </TouchableOpacity>
     );
   };
 
   return (
-    <View>
-      <Text>File List:</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>File List:</Text>
       <FlatList
         data={fileList}
         renderItem={renderFileItem}
@@ -58,5 +63,30 @@ const BucketFilesScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  fileItem: {
+    backgroundColor: "white",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#e32f45",
+  },
+  fileName: {
+    color: "#e32f45",
+    fontSize: 16,
+  },
+});
 
 export default BucketFilesScreen;
