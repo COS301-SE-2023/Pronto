5c88ee86fa3e1071ad210a2f0f177e2aae0064a3
Tyrone Sutherland-MacLeod <105007195+Tymac15@users.noreply.github.com>
2023-06-25 12:46:35 +0200

    Frontend lecturer test (#94)
    * Initial testing
    
    * setting up tests
    
    * progress
    
    * Update LecturerNavigation.test.js
    
    * Update LecturerNavigation.test.js
    
    * progress
    
    * Create postAccordion.test.js
    
    * Update postAccordion.test.js
    
    * updated accordion tests
    
    * additions
    
    * Update postAccordion.test.js
    
    * updates
    
    * updates
    
    * Update PersonalInfo.test.js
    
    * Update Personal-info.js
    
    * Create RecentAnnouncement.test.js
    
    * card tests
    
    * Update RecentAnnouncement.test.js
    
    * fixing tests
    
    * Update postAccordion.test.js
    
    * Update EditModule.test.js
    
    * Tests Updated
    
    Errors Corrected
    All tests pass successfully.
    Data test IDs added to required elements in the DOM
    
    * Update LecturerNavigation.js
    
    * Update RecentAnnouncement.js
    
    * Update RecentAnnouncement.js
    
    * update css
    
    * Update InstitutionNavigation.js
    
    * Update InstitutionNavigation.js
    
    ---------
    
    Co-authored-by: u21598267- J. Albuquerque <u21598267@tuks.co.za>

--- Web view/Web app/src/LectureView/Edit Module/PostAccordion.js
+++ Web view/Web app/src/LectureView/Edit Module/PostAccordion.js
@@ -32,12 +32,13 @@ export default function PostAccordion() {
 
   return (
     <div>
-      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} data-testid={'accordion1'}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{"color":"#e32f45"}} />}
           aria-controls="panel1bh-content"
           id="panel1bh-header"
           style={{"width": "100%"}}
+          data-testid={'accordionDrop1'}
         >
           <Typography sx={{ width: '100%', flexShrink: 0, fontWeight: 'bold',textAlign:"center" }} >
             Post reminder
@@ -46,24 +47,24 @@ export default function PostAccordion() {
         </AccordionSummary>
         <AccordionDetails>
         <form>
-          <div class="form-group row">
-            <label for="colFormLabel" class="col-sm-2 col-form-label">Title: </label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="colFormLabel" required></input>
+          <div className="form-group row">
+            <label htmlFor="colFormLabel" className="col-sm-2 col-form-label">Title: </label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="colFormLabel"  data-testid="title1"  required ></input>
             </div>
           </div>
 
-          <div class="form-group row">
-            <label for="colFormLabel" class="col-sm-2 col-form-label">Body: </label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="colFormLabel" required></input>
+          <div className="form-group row">
+            <label htmlFor="colFormLabel" className="col-sm-2 col-form-label">Body: </label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="colFormLabel"  data-testid="body1" required></input>
             </div>
           </div>
 
-          <div class="form-group row">
-            <label for="colFormLabel" class="col-sm-2 col-form-label">Date: </label>
-            <div class="col-sm-10">
-              <input type="date" class="form-control" id="colFormLabel"></input>
+          <div className="form-group row">
+            <label htmlFor="colFormLabel" className="col-sm-2 col-form-label">Date: </label>
+            <div className="col-sm-10">
+              <input type="date" className="form-control" id="colFormLabel" data-testid="date1"></input>
             </div>
           </div>
 
@@ -72,35 +73,36 @@ export default function PostAccordion() {
         </AccordionDetails>
       </Accordion>
 
-      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')} data-testid={'accordion2'}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{"color":"#e32f45"}}/>}
           aria-controls="panel2bh-content"
           id="panel2bh-header"
+          data-testid={'accordionDrop2'}
         >
           <Typography sx={{ width: '100%', flexShrink: 0, fontWeight: 'bold',textAlign:"center" }}  >Post due assignment</Typography>
           
         </AccordionSummary>
         <AccordionDetails>
         <form>
-          <div class="form-group row">
-            <label for="colFormLabel" class="col-sm-2 col-form-label">Title: </label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="colFormLabel" required></input>
+          <div className="form-group row">
+            <label htmlFor="colFormLabel" className="col-sm-2 col-form-label">Title: </label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="colFormLabel"  data-testid="title2" required></input>
             </div>
           </div>
 
-          <div class="form-group row">
-            <label for="colFormLabel" class="col-sm-2 col-form-label">Information: </label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="colFormLabel"></input>
+          <div className="form-group row">
+            <label htmlFor="colFormLabel" className="col-sm-2 col-form-label">Information: </label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="colFormLabel" data-testid="body2"></input>
             </div>
           </div>
 
-          <div class="form-group row">
-            <label for="colFormLabel" class="col-sm-2 col-form-label">Date: </label>
-            <div class="col-sm-10">
-              <input type="date" class="form-control" id="colFormLabel" required></input>
+          <div className="form-group row">
+            <label htmlFor="colFormLabel" className="col-sm-2 col-form-label">Date: </label>
+            <div className="col-sm-10">
+              <input type="date" className="form-control" id="colFormLabel"  data-testid="date2" required></input>
             </div>
           </div>
 
@@ -109,21 +111,22 @@ export default function PostAccordion() {
         </AccordionDetails>
       </Accordion>
 
-      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')} data-testid={'accordion3'}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{"color":"#e32f45"}}/>}
           aria-controls="panel3bh-content"
           id="panel3bh-header"
+          data-testid={'accordionDrop3'}
         >
           <Typography sx={{ width: '100%', flexShrink: 0, fontWeight: 'bold',textAlign:"center" }}  >Add lecture venue</Typography>
           
         </AccordionSummary>
         <AccordionDetails>
         <form>
-          <div class="form-group row">
-            <label for="colFormLabel" class="col-sm-2 col-form-label">Venue: </label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="colFormLabel" required></input>
+          <div className="form-group row">
+            <label htmlFor="colFormLabel" className="col-sm-2 col-form-label">Venue: </label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="colFormLabel" data-testid="venue" required></input>
             </div>
           </div>
 
@@ -150,11 +153,12 @@ export default function PostAccordion() {
         </AccordionDetails>
       </Accordion>
 
-      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')} data-testid={'accordion4'}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{"color":"#e32f45"}}/>}
           aria-controls="panel4bh-content"
           id="panel4bh-header"
+          data-testid={'accordionDrop4'}
         >
           <Typography sx={{ width: '100%', flexShrink: 0, fontWeight: 'bold',textAlign:"center" }}  >Remove lecture venue</Typography>
           
--- Web view/Web app/src/LectureView/Personal-info.js
+++ Web view/Web app/src/LectureView/Personal-info.js
@@ -16,7 +16,7 @@ const PersonalInfoPage = () => {
 
     return (
       <div style={{ display: 'inline-flex' }}>
-        <nav style={{ width: '20%' }}>
+        <nav style={{ width: '20%' }} data-testid='InstitutionNavigation'>
             {/* Navigation bar content */}
             <LecturerNavigation />
         </nav>
@@ -45,12 +45,13 @@ const PersonalInfoPage = () => {
             </table>
 
             <div>
-                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} data-testid={'paccordion'}>
                 <AccordionSummary
                 expandIcon={<ExpandMoreIcon style={{"color":"#e32f45"}} />}
                 aria-controls="panel1bh-content"
                 id="panel1bh-header"
                 style={{"width": "100%"}}
+                data-testid={'paccordionDrop'}
                 >
                 <Typography sx={{ width: '100%', flexShrink: 0, fontWeight: 'bold',textAlign:"center" }} >
                     Change Password
@@ -62,21 +63,21 @@ const PersonalInfoPage = () => {
                 <div class="form-group row">
                     <label for="colFormLabel" class="col-sm-2 col-form-label">Old password: </label>
                     <div class="col-sm-10">
-                    <input type="text" class="form-control" id="colFormLabel" required></input>
+                    <input type="text" class="form-control" id="colFormLabel" data-testid="pword" required></input>
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label for="colFormLabel" class="col-sm-2 col-form-label">New password: </label>
                     <div class="col-sm-10">
-                    <input type="text" class="form-control" id="colFormLabel" required></input>
+                    <input type="text" class="form-control" id="colFormLabel" data-testid="repword" required></input>
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label for="colFormLabel" class="col-sm-2 col-form-label">Confirm password: </label>
                     <div class="col-sm-10">
-                    <input type="text" class="form-control" id="colFormLabel"></input>
+                    <input type="text" class="form-control" id="colFormLabel" data-testid="newpword"></input>
                     </div>
                 </div>
 
