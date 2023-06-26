5ff5d7038e894e2131a2944fca7a758829e1cc85
u21598267- J. Albuquerque <u21598267@tuks.co.za>
2023-06-26 19:46:32 +0200

    Updated RecentAnnoucement Tests
    Tests have been completed for the RecentAnncoucement component
    - Data-ids have been added
    - comments have been added

--- Web view/Web app/src/__testing__/RecentAnnouncement.test.js
+++ Web view/Web app/src/__testing__/RecentAnnouncement.test.js
@@ -1,34 +1,59 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import RecentAnnouncement from '../LectureView/RecentAnnouncement';
 import {MemoryRouter} from "react-router-dom";
 
 describe('RecentAnnouncement', () => {
 
     beforeEach(() => {
-        render(
-            <MemoryRouter>
-                <RecentAnnouncement/>
-            </MemoryRouter>);
+        render(<MemoryRouter><RecentAnnouncement /></MemoryRouter>);
     });
 
-//create sets of tests for each testID
-
-    // Test if the navigation bar is rendered
-    it('Test if the navigation bar is rendered', () => {
-        const navigation = screen.getByTestId('InstitutionNavigation');
-        expect(navigation).toBeInTheDocument();
+    test('renders institution navigation', () => {
+        const institutionNavigation = screen.getByTestId('institution-navigation');
+        expect(institutionNavigation).toBeInTheDocument();
     });
 
-    //test if cards render
-    it('Test if card 1 is rendered', () => {
-        const card1 = screen.getByTestId('card1');
-        expect(card1).toBeInTheDocument();
+    test('renders announcement cards with options', () => {
+        const announcementCard1 = screen.getByTestId('announcement-card1');
+        const announcementCard2 = screen.getByTestId('announcement-card2');
+        const optionsButton1 = screen.getByTestId('options-button1');
+        const optionsButton2 = screen.getByTestId('options-button2');
+
+        expect(announcementCard1).toBeInTheDocument();
+        expect(announcementCard2).toBeInTheDocument();
+        expect(optionsButton1).toBeInTheDocument();
+        expect(optionsButton2).toBeInTheDocument();
     });
 
-    it('Test if card 2 is rendered', () => {
-        const card2 = screen.getByTestId('card2');
-        expect(card2).toBeInTheDocument();
+    test('opens and closes options menu for announcement card 1', () => {
+        const optionsButton1 = screen.getByTestId('options-button1');
+        fireEvent.click(optionsButton1);
+        const optionsMenu1 = screen.getByTestId('options-menu1');
+
+
+        expect(optionsMenu1).toBeInTheDocument();
+
+
+    });
+
+    test('opens and closes options menu for announcement card 2', () => {
+        const optionsButton2 = screen.getByTestId('options-button2');
+        fireEvent.click(optionsButton2);
+
+        const optionsMenu2 = screen.getByTestId('options-menu2');
+
+
+        expect(optionsMenu2).toBeInTheDocument();
+        fireEvent.click(optionsButton2);
+
+    });
+
+    //provide a test from RecentAnnouncement.js that will test the values that have not been tested yet
+    test('test if the announcement card 1 has the correct title', () => {
+        const announcementCard1 = screen.getByTestId('announcement-card1');
+        expect(announcementCard1).toHaveTextContent('COS13222/05/2023No class from Thursday 1 JunePlease note that due to the complexity of the work, there will be no class on Thursday 1 June. We will continue with the work on Tuesday 6 June.');
+
     });
 
 });
