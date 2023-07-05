3f12cc93d0c9e515861baeb4b1b649c826a53c0a
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-07-05 23:43:31 +0200

    add-to-group unit tests

---
+++ amplify/backend/test/.jest/setEnvVars.js
@@ -0,0 +1,6 @@
+process.env.AppClientId = "APP_CLIENTID";
+process.env.AppClientIdWeb = "WEB_CLIENTID";
+process.env.StudentsGroupName = "STUDENTSGROUPNAME";
+process.env.LecturersGroupName = "LECTURERGROUPNAME";
+process.env.AdminGroupName = "ADMINGROUPNAME";
+process.env.MODULES = "add-to-group";
