5948d39757f82126876d2609aa999f981552ad50
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-07-10 21:49:17 +0200

    Added options to select email, SMS, push notifications
    Added options to select email, SMS, push notifications

--- MobileApp/screens/Notifications/NotificationPreferences.js
+++ MobileApp/screens/Notifications/NotificationPreferences.js
@@ -1,12 +1,76 @@
-import React from "react";
-import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
 
 const NotificationPreferences = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
-    <SafeAreaView>
-      <Text>Notifications</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Notification Preferences</Text>
+
+      <TouchableOpacity
+        style={[
+          styles.option,
+          selectedOption === "email" && styles.selectedOption,
+        ]}
+        onPress={() => handleOptionSelect("email")}
+      >
+        <Text>Email</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[
+          styles.option,
+          selectedOption === "sms" && styles.selectedOption,
+        ]}
+        onPress={() => handleOptionSelect("sms")}
+      >
+        <Text>SMS</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[
+          styles.option,
+          selectedOption === "push" && styles.selectedOption,
+        ]}
+        onPress={() => handleOptionSelect("push")}
+      >
+        <Text>Push Notifications</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 15,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  option: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    padding: 12,
+    marginBottom: 12,
+  },
+  selectedOption: {
+    backgroundColor: "#ccc",
+  },
+});
 
 export default NotificationPreferences;
