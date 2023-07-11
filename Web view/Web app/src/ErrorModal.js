d813368798da9dfb92809601f80d26f016db47fc
u20612894 <u20612894@tuks.co.za>
2023-07-11 10:15:43 +0200

    Error handling
    Added a Modal for displaying error messages.It is to be shared by the lecturer and institution views

---
+++ Web view/Web app/src/ErrorModal.js
@@ -0,0 +1,40 @@
+import { useState} from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position:'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '80%',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+export default function AddModal(errorMessage) {
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = async()=>{ 
+    setOpen(true)
+  }
+
+  const handleClose= async()=>{ 
+    setOpen(false)
+  }
+  <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+            {errorMessage}
+        </Box>
+    </Modal>     
+
+}
