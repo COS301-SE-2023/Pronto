7006531208be29c7c085ba44c5cece6e67febaf2
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-07-11 12:09:28 +0200

    Added new amplify files
    Added new amplify files

--- Web view/Web app/amplify/backend/function/prontoAuthPreConfirmation/src/roles.js
+++
@@ -1,7 +0,0 @@
-
-const ROLES = {
-    Lecture: 'Lecture',
-    Student: 'Student',
-    Admin: 'Admin'
-};
-module.exports = { ROLES };
--- amplify/backend/test/setUp.md
+++
@@ -1,4 +0,0 @@
-# Start Env Environment
-`pipenv shell`
-# Mock API and Other back-end features locally
-`amplify mock`
