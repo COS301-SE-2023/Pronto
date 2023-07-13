1e345bbea1dd79037b8f1d94bd2a1f6ef172bb5f
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-07-13 10:38:57 +0200

    simpler error messages

--- amplify/backend/function/pronto5f713f59PreSignup/src/isAppClientValid.js
+++ amplify/backend/function/pronto5f713f59PreSignup/src/isAppClientValid.js
@@ -1,13 +1,14 @@
 const ROLES = require("./roles.js");
 function isAppClientValid(clientId, role) {
-  if (!(role in ROLES)) throw new Error("Invalid User Role");
+  if (!Object.values(ROLES).includes(role))
+    throw new Error("Invalid User Role");
   switch (clientId) {
     case process.env.AppClientIdWeb:
-      return ROLES.Lecture == role || ROLES.Admin == role;
+      return ROLES.Lecture === role || ROLES.Admin === role;
     case process.env.AppClientId:
-      return ROLES.Student == role;
+      return ROLES.Student === role;
     default:
-      throw new Error(`Unrecognised user pool app client ID=${clientId}`);
+      throw new Error(`Unrecognised user pool app client ID`);
   }
 }
 module.exports = isAppClientValid;
