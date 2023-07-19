be1f55c9949ca0e3b625ac26b7bcf9b8b7d21dc5
u20612894 <u20612894@tuks.co.za>
2023-07-19 13:17:00 +0200

    Fix
    Attempting a fix

--- scripts/configure-amplify.sh
+++ scripts/configure-amplify.sh
@@ -4,39 +4,39 @@ IFS='|'
 
 echo "Secrets are'${AWS_ACCESS_KEY_ID}' '${AWS_SECRET_ACCESS_KEY}' `${APP_ID}`"
 
-AWSCLOUDFORMATIONCONFIG="{\
-\"configLevel\":\"project\",\
-\"useProfile\":true,\
-\"profileName\":\"amplify-dev\",\
-\"accessKeyId\":\"${AWS_ACCESS_KEY_ID}\",\
-\"secretAccessKey\":\"${AWS_SECRET_ACCESS_KEY}\",\
-\"region\":\"us-east-1\"\
-}"
-AMPLIFY="{\
-\"projectName\":\"pronto\",\
-\"appId\":\"${APP_ID}\",\
-\"envName\":\"dev\",\
-\"defaultEditor\":\"Visual Studio Code\"\
-}"
-FRONTEND="{\
-\"frontend\":\"ios\"
-}"
-PROVIDERS="{\
-\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
-}"
+# AWSCLOUDFORMATIONCONFIG="{\
+# \"configLevel\":\"project\",\
+# \"useProfile\":true,\
+# \"profileName\":\"amplify-dev\",\
+# \"accessKeyId\":\"${AWS_ACCESS_KEY_ID}\",\
+# \"secretAccessKey\":\"${AWS_SECRET_ACCESS_KEY}\",\
+# \"region\":\"us-east-1\"\
+# }"
+# AMPLIFY="{\
+# \"projectName\":\"pronto\",\
+# \"appId\":\"${APP_ID}\",\
+# \"envName\":\"dev\",\
+# \"defaultEditor\":\"Visual Studio Code\"\
+# }"
+# FRONTEND="{\
+# \"frontend\":\"ios\"
+# }"
+# PROVIDERS="{\
+# \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+# }"
 
-CODEGEN="{\
-\"generateCode\":true,\
-\"codeLanguage\":\"swift\",\
-\"fileNamePattern\":\"graphql/**/*.graphql\",\
-\"generatedFileName\":\"API.swift\",\
-\"generateDocs\":true,\
-\"maxDepth\":2\
-}"
+# CODEGEN="{\
+# \"generateCode\":true,\
+# \"codeLanguage\":\"swift\",\
+# \"fileNamePattern\":\"graphql/**/*.graphql\",\
+# \"generatedFileName\":\"API.swift\",\
+# \"generateDocs\":true,\
+# \"maxDepth\":2\
+# }"
 
-amplify configure project \
---amplify $AMPLIFY \
---frontend $FRONTEND \
---providers $PROVIDERS \
---codegen $CODEGEN \
---yes
+# amplify configure project \
+# --amplify $AMPLIFY \
+# --frontend $FRONTEND \
+# --providers $PROVIDERS \
+# --codegen $CODEGEN \
+# --yes
