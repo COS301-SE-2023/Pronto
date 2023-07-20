b0f2b8ef36d3c78fcf150de666afc6ec158cc3d0
Tymac15 <105007195+Tymac15@users.noreply.github.com>
2023-07-20 16:39:12 +0200

    Update HomePage.js

--- Web view/Web app/src/HomePage.js
+++ Web view/Web app/src/HomePage.js
@@ -1,23 +1,30 @@
-//import ProntoLogo from "./Authentication/Institution/ProntoLogo.png";
+
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Button, CardActions } from '@mui/material';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 
 function HomePage() {
   return (
 
-      <Container maxWidth="100%">
+    <Container maxWidth="100%" fixed bgcolor = "#e32f45" >
+
+    <Card sx={{maxWidth:2000}}>
+      <CardMedia
+      component="img"
+      height="140"
+      image="./Authentication/Institution/ProntoLogo.png"
+      alt="logo"
+      />
+      <CardContent>
 
       <Grid container spacing={2}>
       <Grid item xs={6}>
-      
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
+        <Card sx={{ maxWidth: 400 }}>
             <CardMedia
               component="img"
               height="140"
@@ -29,10 +36,9 @@ function HomePage() {
                 Institution Admin
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                If you would like to sign into an admin account for a registered Institution, click on the button below:
+                If you would like to sign into an admin account for a registered Institution, click the button below:
               </Typography>
             </CardContent>
-          </CardActionArea>
           <CardActions>
             <Button size="small" color="error" href="/institution-login">
               Sign in
@@ -42,35 +48,34 @@ function HomePage() {
         </Grid>
 
         <Grid item xs={6}>
-      
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="Institution"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h4" component="div">
-              Lecturer
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              If you would like to sign into or create a Lecturer account for a registered institution, click the button below:
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="error" href="/institution-login">
-            Sign in/Register
-          </Button>
-        </CardActions>
-      </Card>
-      </Grid>
+        <Card sx={{ maxWidth: 400 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="Institution"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h4" component="div">
+                Lecturer
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                If you would like to sign into or create a Lecturer account for a registered institution, click the button below:
+              </Typography>
+            </CardContent>
+          <CardActions>
+            <Button size="small" color="error" href="/lecturer-login">
+              Sign in/Register
+            </Button>
+          </CardActions>
+        </Card>
+        </Grid>
 
       </Grid>
+      </CardContent>
+    </Card>
 
-      </Container>
+    </Container>
   );
 }
 export default HomePage;
