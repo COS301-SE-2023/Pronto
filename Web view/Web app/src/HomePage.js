a3e46e4097ad7785cc5b637405402561c1cfbebc
Tymac15 <105007195+Tymac15@users.noreply.github.com>
2023-07-20 15:52:06 +0200

    Update HomePage.js

--- Web view/Web app/src/HomePage.js
+++ Web view/Web app/src/HomePage.js
@@ -1,10 +1,48 @@
-import React, { useState } from "react";
-import ProntoLogo from "./Authentication/Institution/ProntoLogo.png";
+//import ProntoLogo from "./Authentication/Institution/ProntoLogo.png";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function HomePage() {
   return (
-    <h1>Im a beast</h1>
+
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box sx={{ bgcolor: '#FFFFFF', height: '100vh' }} />
+
+        <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="Institution"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Institution
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              If you would like to sign into an admin account for a registered Institution, click the button below:
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Login
+          </Button>
+        </CardActions>
+      </Card>
+
+      </Container>
+    </React.Fragment>
   );
 }
-
 export default HomePage;
