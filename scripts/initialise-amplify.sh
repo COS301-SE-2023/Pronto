faff48a72ee3930fca0b7fb63d3f82bf5d997eb4
u20612894 <u20612894@tuks.co.za>
2023-07-23 21:12:53 +0200

    Fix
    Attempting to fix build

--- scripts/initialise-amplify.sh
+++ scripts/initialise-amplify.sh
@@ -2,29 +2,31 @@
 set -e
 IFS='|'
 
-AWSCLOUDFORMATIONCONFIG="{\
-\"configLevel\":\"project\",\
-\"useProfile\":true,\
-\"profileName\":\"amplify_dev\",\
-\"accessKeyId\":\"${AWS_ACCESS_KEY_ID}\",\
-\"secretAccessKey\":\"${AWS_SECRET_ACCESS_KEY}\",\
-\"region\":\"us-east-1\"\
-}"
-AMPLIFY="{\
-\"projectName\":\"pronto\",\
-\"appId\":\"d3j57fmadrzn3\",\
-\"envName\":\"dev\",\
-\"defaultEditor\":\"code\"\
-}"
-FRONTEND="{\
-\"frontend\":\"ios\"
-}"
-PROVIDERS="{\
-\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
-}"
+echo "'${AWS_ACCESS_KEY_ID}' '${AWS_SECRET_ACCESS_KEY}'"
 
-amplify init \
---amplify $AMPLIFY \
---frontend $FRONTEND \
---providers $PROVIDERS \
---yes
+# AWSCLOUDFORMATIONCONFIG="{\
+# \"configLevel\":\"project\",\
+# \"useProfile\":true,\
+# \"profileName\":\"amplify_dev\",\
+# \"accessKeyId\":\"${AWS_ACCESS_KEY_ID}\",\
+# \"secretAccessKey\":\"${AWS_SECRET_ACCESS_KEY}\",\
+# \"region\":\"us-east-1\"\
+# }"
+# AMPLIFY="{\
+# \"projectName\":\"pronto\",\
+# \"appId\":\"d3j57fmadrzn3\",\
+# \"envName\":\"dev\",\
+# \"defaultEditor\":\"code\"\
+# }"
+# FRONTEND="{\
+# \"frontend\":\"ios\"
+# }"
+# PROVIDERS="{\
+# \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+# }"
+
+# amplify init \
+# --amplify $AMPLIFY \
+# --frontend $FRONTEND \
+# --providers $PROVIDERS \
+# --yes
