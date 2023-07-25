95988106ac098f3f609f3cf3af750a1a75857b69
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-07-25 22:55:05 +0200

    Fixed styling issues
    Fixed styling issues

--- MobileApp/components/StepByStepInstructions.js
+++ MobileApp/components/StepByStepInstructions.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const StepByStepInstructions = ({ instructions }) => {
-    const icon = <Icon name="directions-walk" size={20} color="#000" />;
+    const icon = <Icon name="directions-walk" size={20} color="#e32f45" />;
 
     const [expanded, setExpanded] = useState(true);
 
@@ -30,14 +30,14 @@ const StepByStepInstructions = ({ instructions }) => {
             </TouchableOpacity>
 
             {expanded && (
-                <>
+                <View style={styles.expandedContainer}>
                     {instructions.map((instruction, index) => (
                         <View key={index} style={styles.instructionContainer}>
                             {icon}
                             <Text style={styles.instructionText}>{instruction}</Text>
                         </View>
                     ))}
-                </>
+                </View>
             )}
         </View>
     );
@@ -45,19 +45,25 @@ const StepByStepInstructions = ({ instructions }) => {
 
 const styles = StyleSheet.create({
     container: {
+        position: 'absolute',
+        bottom: "20%", // Adjust this value to control the vertical position
+        right: 10, // Adjust this value to control the horizontal position
         padding: 16,
         backgroundColor: '#fff',
-        marginTop: 8,
         borderRadius: 8,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.2,
         shadowRadius: 2,
         elevation: 3,
+        marginLeft: 30,
     },
     iconContainer: {
         flexDirection: 'row',
         alignItems: 'center',
+    },
+    expandedContainer: {
+        marginTop: 10, // Add some space between the icon and the expanded directions
     },
     instructionContainer: {
         flexDirection: 'row',
@@ -71,7 +77,9 @@ const styles = StyleSheet.create({
     showDirectionsText: {
         fontSize: 14,
         marginLeft: 4,
+        marginBottom: 4,
         color: '#000',
+        fontWeight: 'bold',
     },
 });
 
