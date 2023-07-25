a039b46713221fa65d0f0b88e120e55c226a46b1
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-07-25 18:41:08 +0200

    Updated error handling
    Updated error handling

--- MobileApp/screens/Timetable/ScheduleTable.js
+++ MobileApp/screens/Timetable/ScheduleTable.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { View, TouchableOpacity, Text, Dimensions } from "react-native";
+import { View, TouchableOpacity, Text, Dimensions, Alert } from "react-native";
 import { Agenda } from "react-native-calendars";
 import { Card } from "react-native-paper";
-import{API,Auth} from 'aws-amplify'
-import{listStudents} from "../../graphql/queries"
+import { API, Auth } from 'aws-amplify'
+import { listStudents } from "../../graphql/queries"
 import { createStudent } from "../../graphql/mutations";
 
-const ScheduleTable= () => {
+const ScheduleTable = () => {
 
- // const[student,setStudent]=useState(null)
-  const[activities,setActivities]=useState([])
-  const[schedule,setSchedule]=useState(null)
-  var scheduleArray={}
-  
-//function to take in a day, and give all dates of the year that a day occurs
-  const getDatesForDayOfWeek= (dayOfWeek)=>{
+  // const[student,setStudent]=useState(null)
+  const [activities, setActivities] = useState([])
+  const [schedule, setSchedule] = useState(null)
+  var scheduleArray = {}
+
+  //function to take in a day, and give all dates of the year that a day occurs
+  const getDatesForDayOfWeek = (dayOfWeek) => {
     const date = new Date();
     const year = date.getFullYear();
     const dayIndex = [
@@ -27,17 +27,17 @@ const ScheduleTable= () => {
       "Saturday",
     ].indexOf(dayOfWeek);
     const results = [];
-      let month=date.getMonth()
-  // Loop through each month of the year
-    for ( month; month < 12; month++) {
-    // Create a new date object for the first day of the month
+    let month = date.getMonth()
+    // Loop through each month of the year
+    for (month; month < 12; month++) {
+      // Create a new date object for the first day of the month
       const firstDayOfMonth = new Date(year, month, 1);
 
-    // Find the first occurrence of the specified day of the week
+      // Find the first occurrence of the specified day of the week
       const diff = dayIndex - firstDayOfMonth.getDay();
       let dayOfMonth = diff >= 0 ? diff + 1 : diff + 8;
 
-    // Loop through the rest of the month, adding dates for the specified day of the week
+      // Loop through the rest of the month, adding dates for the specified day of the week
       while (dayOfMonth <= new Date(year, month + 1, 0).getDate()) {
         const dateString = `${year}-${(month + 1)
           .toString()
@@ -49,125 +49,127 @@ const ScheduleTable= () => {
     return results;
   }
 
-  const fetchActivities= async()=>{
-    try{ 
-        let user=await Auth.currentAuthenticatedUser()
-        let studentEmail=user.attributes.email;
+  let error = "There seem to be network problems. Try again later";
 
-        let act=[]
-        let error ="There seem tp be network problems.Try again later"
-        let stu=await API.graphql({
-                    query:listStudents,
-                    variables:{
-                        filter :{ 
-                            email : { 
-                                eq:studentEmail
-                            }
-                        }
-                    } ,
-                    authMode:"AMAZON_COGNITO_USER_POOLS"                
-                  })
-                
-        //Student does not exist so create them
-        if(stu.data.listStudents.items.length===0){
-          let domain=studentEmail.split("@")[1]
-          
-          //Find Institution via domain
-          let institution=await API.graphql({
-                        query:listInstitutions,
-                        variables:{ 
-                                filter :{ 
-                                  domains :{ 
-                                    contains:domain
-                                }
-                            }
-                        },
-                        authMode:"API_KEY",
-                    })
+  const fetchActivities = async () => {
+    try {
+      let user = await Auth.currentAuthenticatedUser()
+      let studentEmail = user.attributes.email;
 
-          //Institution not found
-          if(institution.data.listInstitutions.items.length===0){
-            error= "Could not determine institution"
-            throw Error()
-          }
-    
-          institution=institution.data.listInstitutions.items[0]
-          
-          //Create student
-          let newStudent={
-              institutionId:institution.id,
-              firstname: user.attributes.name,
-              lastname: user.attributes.family_name,
-              userRole: "Student",
-              email: studentEmail   
+      let act = []
+
+      let stu = await API.graphql({
+        query: listStudents,
+        variables: {
+          filter: {
+            email: {
+              eq: studentEmail
             }
-
-          let create=await API.graphql({
-                    query:createStudent,
-                    variables:{input:newStudent},
-                    authMode:"AMAZON_COGNITO_USER_POOLS"
-            })
-              stu=create.data.createStudent
           }
-        
-          //Student  found
-          else{
-            stu=stu.data.listStudents.items[0]                    
-            let c=[]
-            for(let i=0;i<stu.enrollments.items.length;i++){
-              c.push(stu.enrollments.items[i].course)
-            }
-             
-            if(stu.timetable!==null){
-              for(let i=0;i<stu.timetable.activityId.length;i++){
-                for(let j=0;j<c.length;j++){
-                  let index=c[j].activity.items.find(item=>item.id===stu.timetable.activityId[i])
-                    if(index!==undefined){
-                      act.push(index)
-                      break;
-                    }
-                }
+        },
+        authMode: "AMAZON_COGNITO_USER_POOLS"
+      })
+
+      //Student does not exist so create them
+      if (stu.data.listStudents.items.length === 0) {
+        let domain = studentEmail.split("@")[1]
+
+        //Find Institution via domain
+        let institution = await API.graphql({
+          query: listInstitutions,
+          variables: {
+            filter: {
+              domains: {
+                contains: domain
               }
-              act=act.sort((a, b) => {
-                    if(a.start<=b.start)
-                      return -1
-                    else 
-                      return 1   
-                  })
+            }
+          },
+          authMode: "API_KEY",
+        })
 
-                let changed = false
-                if(act.length===activities.length){
-                  for(let i=0;i<act.length ;i++){
-                    if(act[i].id!==activities[i].id){
-                        changed=true
-                        break
-                    }  
-                 }
-                }
-                else{
-                  changed=true
-                }
+        //Institution not found
+        if (institution.data.listInstitutions.items.length === 0) {
+          error = "Could not determine institution"
+          throw Error()
+        }
 
-                if(changed){
-                  //setStudent(stu)
-                  setActivities(act)
-                  createScheduleArray(act)
-                }
-             }
-          }
-      }catch(e){
-        Alert(error)
+        institution = institution.data.listInstitutions.items[0]
+
+        //Create student
+        let newStudent = {
+          institutionId: institution.id,
+          firstname: user.attributes.name,
+          lastname: user.attributes.family_name,
+          userRole: "Student",
+          email: studentEmail
+        }
+
+        let create = await API.graphql({
+          query: createStudent,
+          variables: { input: newStudent },
+          authMode: "AMAZON_COGNITO_USER_POOLS"
+        })
+        stu = create.data.createStudent
       }
+
+      //Student  found
+      else {
+        stu = stu.data.listStudents.items[0]
+        let c = []
+        for (let i = 0; i < stu.enrollments.items.length; i++) {
+          c.push(stu.enrollments.items[i].course)
+        }
+
+        if (stu.timetable !== null) {
+          for (let i = 0; i < stu.timetable.activityId.length; i++) {
+            for (let j = 0; j < c.length; j++) {
+              let index = c[j].activity.items.find(item => item.id === stu.timetable.activityId[i])
+              if (index !== undefined) {
+                act.push(index)
+                break;
+              }
+            }
+          }
+          act = act.sort((a, b) => {
+            if (a.start <= b.start)
+              return -1
+            else
+              return 1
+          })
+
+          let changed = false
+          if (act.length === activities.length) {
+            for (let i = 0; i < act.length; i++) {
+              if (act[i].id !== activities[i].id) {
+                changed = true
+                break
+              }
+            }
+          }
+          else {
+            changed = true
+          }
+
+          if (changed) {
+            //setStudent(stu)
+            setActivities(act)
+            createScheduleArray(act)
+          }
+        }
+      }
+    } catch (e) {
+      Alert.alert(error)
+    }
   }
 
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchActivities()
-  },[])
- 
+  }, [])
+
   fetchActivities()
 
-  const createScheduleArray= async(modules)=>{
+  const createScheduleArray = async (modules) => {
     scheduleArray = {};
     for (const moduleKey in modules) {
       const dates = getDatesForDayOfWeek(modules[moduleKey].day);
@@ -175,7 +177,7 @@ const ScheduleTable= () => {
         if (!scheduleArray[date]) {
           scheduleArray[date] = [];
         }
-        let t =modules[moduleKey].start+"-"+modules[moduleKey].end
+        let t = modules[moduleKey].start + "-" + modules[moduleKey].end
         scheduleArray[date].push({
           id: modules[moduleKey].id,
           code: modules[moduleKey].course.coursecode,
@@ -188,7 +190,7 @@ const ScheduleTable= () => {
 
       });
     }
-  
+
     setSchedule(scheduleArray)
   }
 
@@ -230,7 +232,7 @@ const ScheduleTable= () => {
             fontSize: 25,
             color: "#748c94",
           }}
-          // #1E90FF
+        // #1E90FF
         >
           No events today
         </Text>
@@ -259,15 +261,15 @@ const ScheduleTable= () => {
           agendaDayTextColor: "#e32f45",
           agendaDayNumColor: "#e32f45",
           agendaTodayColor: "#e32f45",
-  //         //further styling options if needed
-  //         //  textDisabledColor: "#e32f45",
-  //         //   agendaTodayColor: "#e32f45", // today in list
-  //         //   monthTextColor: "#e32f45", // name in calendar
-  //         //    textDefaultColor: "#e32f45",
-  //         // todayBackgroundColor: "#e32f45",
-  //         // selectedDayTextColor: "#e32f45", // calendar sel date
-  //         //  dayTextColor: "#e32f45", // calendar day
-  //         //  textSectionTitleColor: "#e32f45",
+          //         //further styling options if needed
+          //         //  textDisabledColor: "#e32f45",
+          //         //   agendaTodayColor: "#e32f45", // today in list
+          //         //   monthTextColor: "#e32f45", // name in calendar
+          //         //    textDefaultColor: "#e32f45",
+          //         // todayBackgroundColor: "#e32f45",
+          //         // selectedDayTextColor: "#e32f45", // calendar sel date
+          //         //  dayTextColor: "#e32f45", // calendar day
+          //         //  textSectionTitleColor: "#e32f45",
         }}
       />
     </View>
