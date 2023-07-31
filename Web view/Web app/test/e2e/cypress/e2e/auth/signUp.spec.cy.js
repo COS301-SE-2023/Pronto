625dd58fcc71788bccd8ce8bc106ed5ae06cffa2
Bandisa Masilela <bandisa@Bandisas-MacBook-Air.local>
2023-07-31 03:44:13 +0200

    lecturer sign test: is lecturer part of inst

--- Web view/Web app/test/e2e/cypress/e2e/auth/signUp.spec.cy.js
+++ Web view/Web app/test/e2e/cypress/e2e/auth/signUp.spec.cy.js
@@ -27,4 +27,25 @@ describe("Testing sign up input and error handling", () => {
     cy.get("button[type=button]").eq(1).click();
   });
 
+  it(`should test if lecturer email is part of institution`, function () {
+    cy.createTestUser();
+    cy.get("button[type=button]").eq(1).click();
+
+    const lectureTestUser = this.users.lecturers.pop();
+    cy.get("input[placeholder=Name").eq(0).type(lectureTestUser.name);
+    cy.get("input[placeholder=Surname").type(lectureTestUser.surname);
+    cy.get("input[type=email]").eq(0).type(lectureTestUser.email);
+    cy.get(`input[class^=${this.reactSelectAttributes.classNamePrefix}]`).type(
+      `${lectureTestUser.institutionName}{enter}`
+    );
+    cy.get("input[type=password]").eq(0).type(lectureTestUser.password);
+    cy.get("input[type=password]").eq(1).type(lectureTestUser.password);
+    cy.get("button[type=submit").eq(0).click({ force: true });
+
+    //assert error message
+    cy.xpath('//*[@id="root"]/div[2]/div/div[1]/form/p', {
+      timeout: 5 * 1000,
+    }).should("exist");
+    cy.contains(this.authErrors.LECTUEREREMAILERROR);
+  });
 });
