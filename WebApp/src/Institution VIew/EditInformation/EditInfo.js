52b16e30d9f2efe09aa34fcef7c7060a69322c36
u20612894 <u20612894@tuks.co.za>
2023-08-10 14:04:07 +0200

    User State
    Using a user state to manage user info

--- WebApp/src/Institution VIew/EditInformation/EditInfo.js
+++ WebApp/src/Institution VIew/EditInformation/EditInfo.js
@@ -5,11 +5,12 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {Auth} from 'aws-amplify'
 import '../Navigation/Navigation.css';
 
 const EditInfoPage = () => {
     const [expanded, setExpanded] = React.useState(false);
-
+    const[user,setUser]=React.useState(null)
     const handleChange = (panel) => (event, isExpanded) => {
         setExpanded(isExpanded ? panel : false);
     };
