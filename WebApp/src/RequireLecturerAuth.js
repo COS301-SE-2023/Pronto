67b37027737ef3f0329d0a2a1b93363cb132b82c
Tymac15 <u21578878@tuks.co.za>
2023-08-11 18:25:38 +0200

    created lecturer auth

--- WebApp/src/RequireLecturerAuth.js
+++ WebApp/src/RequireLecturerAuth.js
@@ -1,11 +1,22 @@
 import { useLocation, Navigate } from 'react-router-dom';
 import { Auth } from "aws-amplify";
 
+const [user, setUser] = useState(undefined);
+const [userGroup, setUserGroup] = useState(null);
+
 export async function RequireLecturerAuth({ children }) {
-  const location = useLocation();
-  const { route } = await Auth.currentAuthenticatedUser({});;
-  if (route !== 'authenticated'|| (route.signInUserSession.idToken.payload["cognito:groups"] != "lecturerUserGroup")) {
-    return <Navigate to="/lecturer-login" state={{ from: location }} replace />;
-  }
-  return children;
-}
+    const location = useLocation();
+    try {
+      const authUser = await Auth.currentAuthenticatedUser({});
+      const group =
+        authUser.signInUserSession.idToken.payload["cognito:groups"];
+      setUser(authUser);
+      setUserGroup(group);
+    } catch (e) {
+      setUser(null);
+    }
+    if( (!user) || (userGroup != "lecturerUserGroup") ){
+        return <Navigate to="/institution-login" state={{ from: location }} replace />;
+    }
+    return children;
+  };
