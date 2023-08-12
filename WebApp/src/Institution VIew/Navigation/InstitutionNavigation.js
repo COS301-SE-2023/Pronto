bdf25bb5eae355b385a8c6e08220f74d675e7ca2
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-08-12 14:19:44 +0200

    Finalises style of navbar
    Finalises style of navbar

--- WebApp/src/Institution VIew/Navigation/InstitutionNavigation.js
+++ WebApp/src/Institution VIew/Navigation/InstitutionNavigation.js
@@ -5,67 +5,77 @@ import { Auth } from "aws-amplify";
 import { useNavigate } from "react-router-dom";
 
 export default function InstitutionNavigation() {
-  const navigate = useNavigate();
-  const onSignOut = async (event) => {
-    event.preventDefault();
-    try {
-      await Auth.signOut();
-      //navigate to lecturer login
-      navigate("/institution-login");
-    } catch (e) {
-      console.log(e.message);
-    }
-  };
+    const navigate = useNavigate();
+    const onSignOut = async (event) => {
+        event.preventDefault();
+        try {
+            await Auth.signOut();
+            //navigate to lecturer login
+            navigate("/institution-login");
+        } catch (e) {
+            console.log(e.message);
+        }
+    };
 
     return (
         <div className={'grid'}>
             <nav className="vertical-navbar col-4 p-4" >
                 <div className="top">
-                <img
-                src={logo}
-                alt="Logo"
-                className="logo offset-2 img-fluid mr-1"
-                width={"175px"}
-                height={"155px"}
-                data-testid={'UniversityImage'}
-                />
+                    <img
+                        src={logo}
+                        alt="Logo"
+                        className="logo offset-2 img-fluid mr-1"
+                        width={"175px"}
+                        height={"155px"}
+                        data-testid={'UniversityImage'}
+                    />
                 </div>
-                <ul className="navbar-nav">
-        
-                    <li className="nav-item text-center" data-testid={'Dashboard'}>
-                        <a href="/dashboard" className="nav-link" data-testid={'dashboardLink'}>
-                            <b>Dashboard</b>
-                        </a>
-                    </li>
-                    <li className="nav-item text-center" data-testid={'UploadSchedule'}>
-                        <a href="/upload-schedule" className="nav-link" data-testid={'UploadScheduleLink'}>
-                           <b>Upload Schedule</b>
-                        </a>
-                    </li>
-                    <li className="nav-item text-center" data-testid={'UploadStudentFiles'}>
-                        <a href="/upload-student-files" className="nav-link p-2" data-testid={'UploadStudentFilesLink'}>
-                            <b>Upload Student Files</b>
+                <div className="nav-links-container">
+                    <ul className="navbar-nav">
 
-                        </a>
-                    </li>
-                    <li className="nav-item text-center" data-testid={'AddLecturer'}>
-                        <a href="/add-lecturer" className="nav-link" data-testid={'AddLecturerLink'}>
-                            <b>Add/Remove Lecturer</b>
-                        </a>
-                    </li>
-                    <li className="nav-item text-center" data-testid={'EditUniversityInfo'}>
-                        <a href="/edit-university-info" className="nav-link" data-testid={'EditUniversityInfoLink'}>
-                            <b>Edit University Info</b>
-                        </a>
-                    </li>
+                        <li className="nav-item text-center" data-testid={'Dashboard'}>
+                            <a href="/dashboard" className="nav-link" data-testid={'dashboardLink'}>
+                                <b>Dashboard</b>
+                            </a>
+                        </li>
+                        <li className="nav-item text-center" data-testid={'UploadSchedule'}>
+                            <a href="/upload-schedule" className="nav-link" data-testid={'UploadScheduleLink'}>
+                                <b>Upload Schedule</b>
+                            </a>
+                        </li>
+                        <li className="nav-item text-center" data-testid={'UploadStudentFiles'}>
+                            <a href="/upload-student-files" className="nav-link p-2" data-testid={'UploadStudentFilesLink'}>
+                                <b>Upload Student Files</b>
 
-                </ul>
+                            </a>
+                        </li>
+                        <li className="nav-item text-center" data-testid={'AddLecturer'}>
+                            <a href="/add-lecturer" className="nav-link" data-testid={'AddLecturerLink'}>
+                                <b>Add/Remove Lecturer</b>
+                            </a>
+                        </li>
+                        <li className="nav-item text-center" data-testid={'EditUniversityInfo'}>
+                            <a href="/edit-university-info" className="nav-link" data-testid={'EditUniversityInfoLink'}>
+                                <b>Edit University Info</b>
+                            </a>
+                        </li>
 
+                    </ul>
+                </div>
+                <div className="logoutbtn">
+                    <button
+                        className="btn btn-danger btn-lg btn-block"
+                        style={{ borderRadius: "25px" }}
+                        data-testid="LogoutButton"
+                        onClick={onSignOut}
+                    >
+                        Log Out
+                    </button>
+                </div>
             </nav>
-            <div className="logoutbtn fixed-bottom col-2 p-4 ml-4">
-                <button className={"btn btn-danger btn-lg btn-block"} style={{borderRadius:"25px"}} data-testid={'LogoutButton'} onClick={onSignOut}>Log Out</button>
-            </div>
+
+
         </div>
 
-            );
+    );
 }
