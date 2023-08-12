fd45d97a632c7e3b8320029abe5a72f4b47ec90e
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-08-12 14:40:58 +0200

    Finalised dashboard styling
    Finalised dashboard styling

--- WebApp/src/Institution VIew/Dashboard/Dashboard.js
+++ WebApp/src/Institution VIew/Dashboard/Dashboard.js
@@ -13,25 +13,35 @@ const Dashboard = () => {
                 <InstitutionNavigation />
             </LeftContainer>
             <RightContainer>
+
                 <div className={"grid"} style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100%" }}>
+                    <h1 style={{ borderBottom: "2px solid black" }}>Dashboard</h1>
                     <div className={"row ml-3"} style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
                         <div className={"col-6"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                            <DriveFolderUploadIcon style={{ fontSize: "250px", cursor: "pointer" }} onClick={() => { window.location.href = "/upload-schedule"; }} />
-                            <span style={{ fontSize: "50px" }}>Upload Schedule</span>
+                            <HoverableIcon>
+                                <DriveFolderUploadIcon style={{ fontSize: "200px", cursor: "pointer" }} onClick={() => { window.location.href = "/upload-schedule"; }} />
+                                <span style={{ fontSize: "40px" }}>Upload Schedule</span>
+                            </HoverableIcon>
                         </div>
                         <div className={"col-6"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                            <CloudUploadIcon style={{ fontSize: "250px", cursor: "pointer" }} onClick={() => { window.location.href = "/upload-student-files"; }} />
-                            <span style={{ fontSize: "50px" }}>Upload File</span>
+                            <HoverableIcon>
+                                <CloudUploadIcon style={{ fontSize: "200px", cursor: "pointer" }} onClick={() => { window.location.href = "/upload-student-files"; }} />
+                                <span style={{ fontSize: "40px" }}>Upload File</span>
+                            </HoverableIcon>
                         </div>
                     </div>
                     <div className={"row ml-3"} style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
                         <div className={"col-6"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                            <SchoolIcon style={{ fontSize: "250px", cursor: "pointer" }} onClick={() => { window.location.href = "/add-lecturer"; }} />
-                            <span style={{ fontSize: "50px" }}>Add/Remove Lecturer</span>
+                            <HoverableIcon>
+                                <SchoolIcon style={{ fontSize: "200px", cursor: "pointer" }} onClick={() => { window.location.href = "/add-lecturer"; }} />
+                                <span style={{ fontSize: "40px" }}>Add/Remove Lecturer</span>
+                            </HoverableIcon>
                         </div>
                         <div className={"col-6"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                            <EditIcon style={{ fontSize: "250px", cursor: "pointer" }} onClick={() => { window.location.href = "/edit-university-info"; }} />
-                            <span style={{ fontSize: "50px" }}>Edit University Info</span>
+                            <HoverableIcon>
+                                <EditIcon style={{ fontSize: "200px", cursor: "pointer" }} onClick={() => { window.location.href = "/edit-university-info"; }} />
+                                <span style={{ fontSize: "40px" }}>Edit University Info</span>
+                            </HoverableIcon>
                         </div>
                     </div>
                 </div>
@@ -56,6 +66,17 @@ const RightContainer = styled.div`
   right: 0;
   width: 80%;
   z-index: 2;
+`;
+
+const HoverableIcon = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+    color: #e32f45;
+  }
 `;
 
 export default Dashboard;
