36e20c4f806180fcc15613f4b9b927b68e896aec
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-08-12 15:27:33 +0200

    Fixed styling of edit university info page
    Fixed styling of edit university info page

--- WebApp/src/Institution VIew/EditInformation/EditInfo.js
+++ WebApp/src/Institution VIew/EditInformation/EditInfo.js
@@ -15,83 +15,83 @@ const EditInfoPage = () => {
     };
 
     return (
-      <div style={{ display: 'inline-flex' }}>
-        <nav style={{ width: '20%' }} data-testid='InstitutionNavigation'>
-            {/* Navigation bar content */}
-            <InstitutionNavigation />
-        </nav>
-  
-        <main style={{ width: '900px',marginTop: '30px' }}>
-            <h1 className="moduleHead">Edit University Information</h1>
-            <table class="table table-sm">
-                <tbody>
-                    
-                    <tr>
-                    <td>Institution name:</td>
-                    <td>University of Pretoria</td>
-                    </tr>
+        <div style={{ display: 'inline-flex' }}>
+            <nav style={{ width: '20%' }} data-testid='InstitutionNavigation'>
+                {/* Navigation bar content */}
+                <InstitutionNavigation />
+            </nav>
 
-                    <tr>
-                    <td>Role:</td>
-                    <td>Admin</td>
-                    </tr>
+            <main style={{ width: '900px', marginTop: '30px' }}>
+                <h1 className="moduleHead">Edit University Information</h1>
+                <table class="table table-sm">
+                    <tbody>
 
-                    <tr>
-                    <td>Email address:</td>
-                    <td>ProntoAdmin@up.ac.za</td>
-                    </tr>
-            
-                </tbody>
-            </table>
+                        <tr>
+                            <td>Institution name:</td>
+                            <td>University of Pretoria</td>
+                        </tr>
 
-            <div>
-                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} data-testid={'paccordion'}>
-                <AccordionSummary
-                expandIcon={<ExpandMoreIcon style={{"color":"#e32f45"}} />}
-                aria-controls="panel1bh-content"
-                id="panel1bh-header"
-                style={{"width": "100%"}}
-                data-testid={'paccordionDrop'}
-                >
-                <Typography sx={{ width: '100%', flexShrink: 0, fontWeight: 'bold',textAlign:"center" }} >
-                    Change Password
-                </Typography>
-                
-                </AccordionSummary>
-                <AccordionDetails>
-                <form>
-                <div class="form-group row">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">Old password: </label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" id="colFormLabel" data-testid="pword" required></input>
-                    </div>
+                        <tr>
+                            <td>Role:</td>
+                            <td>Admin</td>
+                        </tr>
+
+                        <tr>
+                            <td>Email address:</td>
+                            <td>ProntoAdmin@up.ac.za</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+                <div>
+                    <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} data-testid={'paccordion'}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon style={{ "color": "#e32f45" }} />}
+                            aria-controls="panel1bh-content"
+                            id="panel1bh-header"
+                            style={{ "width": "100%" }}
+                            data-testid={'paccordionDrop'}
+                        >
+                            <Typography sx={{ width: '100%', flexShrink: 0, fontWeight: 'bold', textAlign: "center" }} >
+                                Change Password
+                            </Typography>
+
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <form>
+                                <div class="form-group row">
+                                    <label for="colFormLabel" class="col-sm-2 col-form-label">Old password: </label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="colFormLabel" data-testid="pword" required></input>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="colFormLabel" class="col-sm-2 col-form-label">New password: </label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="colFormLabel" data-testid="repword" required></input>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="colFormLabel" class="col-sm-2 col-form-label">Confirm password: </label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="colFormLabel" data-testid="newpword"></input>
+                                    </div>
+                                </div>
+
+                                <button className="post-button button-no-border">Update</button>
+                            </form>
+                        </AccordionDetails>
+                    </Accordion>
                 </div>
 
-                <div class="form-group row">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">New password: </label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" id="colFormLabel" data-testid="repword" required></input>
-                    </div>
-                </div>
 
-                <div class="form-group row">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">Confirm password: </label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" id="colFormLabel" data-testid="newpword"></input>
-                    </div>
-                </div>
+            </main>
 
-                    <button className="post-button">Update</button>
-                </form>
-                </AccordionDetails>
-                </Accordion>
-            </div>
-
-          
-        </main>
-  
-      </div>
+        </div>
     );
-  };
-  
-  export default EditInfoPage;
+};
+
+export default EditInfoPage;
