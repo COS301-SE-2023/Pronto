a9bc53c23c32e415626e1e5a451ec0f46977b822
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-08-12 14:33:50 +0200

    Fixed spacing issue on dashboard
    Fixed spacing issue on dashboard

--- WebApp/src/Institution VIew/Dashboard/Dashboard.js
+++ WebApp/src/Institution VIew/Dashboard/Dashboard.js
@@ -7,64 +7,37 @@ import SchoolIcon from '@mui/icons-material/School';
 import EditIcon from '@mui/icons-material/Edit';
 
 const Dashboard = () => {
-    return (<div>
+    return (
+        <div>
             <LeftContainer>
-                <InstitutionNavigation/>
+                <InstitutionNavigation />
             </LeftContainer>
             <RightContainer>
-                <div className={"grid"}>
-
-                    <div className={"row ml-3"}>
-                        <div className={"col-6"} style={{
-                            display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"
-                        }}>
-                            <DriveFolderUploadIcon style={{fontSize: "250px", cursor: "pointer"}}
-                                                   onClick={() => {
-                                                       window.location.href = "/upload-schedule";
-                                                   }}
-                            />
-                            <span style={{fontSize: "50px"}}>Upload Schedule</span>
+                <div className={"grid"} style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100%" }}>
+                    <div className={"row ml-3"} style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+                        <div className={"col-6"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <DriveFolderUploadIcon style={{ fontSize: "250px", cursor: "pointer" }} onClick={() => { window.location.href = "/upload-schedule"; }} />
+                            <span style={{ fontSize: "50px" }}>Upload Schedule</span>
                         </div>
-                        <div className={"col-6"} style={{
-                            display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"
-                        }}>
-                            <CloudUploadIcon style={{fontSize: "250px", cursor: "pointer"}}
-                                             onClick={() => {
-                                                 window.location.href = "/upload-student-files";
-                                             }}
-                            />
-                            <span style={{fontSize: "50px"}}>Upload File</span>
+                        <div className={"col-6"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <CloudUploadIcon style={{ fontSize: "250px", cursor: "pointer" }} onClick={() => { window.location.href = "/upload-student-files"; }} />
+                            <span style={{ fontSize: "50px" }}>Upload File</span>
                         </div>
                     </div>
-
-                    <div className={"row ml-3"}>
-                        <div className={"col-6"} style={{
-                            display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"
-                        }}>
-                            <SchoolIcon style={{fontSize: "250px", "cursor": "pointer"}}
-                                        onClick={() => {
-                                            window.location.href = "/add-lecturer";
-                                        }}
-                            />
-                            <span style={{fontSize: "50px"}}>Add/Remove Lecturer</span>
+                    <div className={"row ml-3"} style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+                        <div className={"col-6"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <SchoolIcon style={{ fontSize: "250px", cursor: "pointer" }} onClick={() => { window.location.href = "/add-lecturer"; }} />
+                            <span style={{ fontSize: "50px" }}>Add/Remove Lecturer</span>
                         </div>
-                        <div className={"col-6"} style={{
-                            display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"
-                        }}>
-                            <EditIcon style={{fontSize: "250px", cursor: "pointer"}}
-                                      onClick={() => {
-                                          window.location.href = "/edit-university-info";
-                                      }}
-                            />
-                            <span style={{fontSize: "50px"}}>Upload File</span>
+                        <div className={"col-6"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <EditIcon style={{ fontSize: "250px", cursor: "pointer" }} onClick={() => { window.location.href = "/edit-university-info"; }} />
+                            <span style={{ fontSize: "50px" }}>Edit University Info</span>
                         </div>
                     </div>
                 </div>
             </RightContainer>
         </div>
-
     );
-
 };
 
 const LeftContainer = styled.div`
@@ -74,7 +47,6 @@ const LeftContainer = styled.div`
   left: 0;
   width: 30%;
   z-index: 2;
-
 `;
 
 const RightContainer = styled.div`
@@ -84,7 +56,6 @@ const RightContainer = styled.div`
   right: 0;
   width: 80%;
   z-index: 2;
-
 `;
 
 export default Dashboard;
