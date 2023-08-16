0ad47469d5f43f7e38adc920a02ba4a1ae42f3e7
Tymac15 <u21578878@tuks.co.za>
2023-08-16 14:37:47 +0200

    Update RequireAdminAuth.js
    tried to debug but still has some issues. commiting for further investigation

--- WebApp/src/RequireAdminAuth.js
+++ WebApp/src/RequireAdminAuth.js
@@ -1,42 +1,39 @@
-import { useState, useEffect } from "react";
-import { useLocation, Navigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { Navigate } from 'react-router-dom';
 import { Auth } from "aws-amplify";
 
 
 export function RequireAdminAuth({ children }) {
 
-  const [user, setUser] = useState(undefined);
-  const [userGroup, setUserGroup] = useState(null);
-  const location = useLocation();
+  let User = null;
+  let Group = null; //I used let instead of const so that we can make these variables global whilst 
+                    // also being able to assign them values in the async function. with the if statement
+                     // in the try, the way it is, these could be declared const within the try, but i have
+                     //left them here global incase we put the if statement outside the try again.
 
   const checkUser = async () => {
     try {
-      const authUser = await Auth.currentAuthenticatedUser({});
+      User = await Auth.currentAuthenticatedUser({});
+      Group = User.signInUserSession.idToken.payload["cognito:groups"][0];
 
+      console.log({ User });
+      console.log({ Group })
 
-      setUser(authUser);
-
-      const group = authUser.signInUserSession.idToken.payload["cognito:groups"][0];
-      setUserGroup(group);
-      console.log({ authUser });
-      console.log({ group })
-
+      if ((!{User}) || ({Group} != "adminUserGroup")) {
+        return <Navigate to="/institution-login"/>;
+      }
+      else 
+      {
+        return {children}; //else returns children here now so that it doesnt automatically get returned
+      }
     } catch (e) {
-      setUser(null);
       console.log("catch ran");
       console.log(e.message);
+      return <Navigate to="/institution-login" />; //if an error is caught return to the login
     }
   };
   useEffect(() => {
     checkUser();
   }, []);
 
-
-  console.log(userGroup)
-
-  if ((!user) || (userGroup !== "adminUserGroup")) {
-    return <Navigate to="/institution-login" state={{ from: location }} replace />;
-  }
-
-  return children;
 };
