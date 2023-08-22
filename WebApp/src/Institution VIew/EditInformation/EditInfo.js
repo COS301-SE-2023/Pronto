22125a5ad786d5b7ae766805f69776f3dd504971
Tymac15 <u21578878@tuks.co.za>
2023-08-22 21:16:26 +0200

    Update EditInfo.js

--- WebApp/src/Institution VIew/EditInformation/EditInfo.js
+++ WebApp/src/Institution VIew/EditInformation/EditInfo.js
@@ -7,7 +7,7 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import '../Navigation/Navigation.css';
 import HelpButton from '../../HelpButton';
-import UserManual from "../HelpFiles/InstitutionInstructions.pdf";
+import UserManual from "../HelpFiles/EditInfo.pdf";
 
 const EditInfoPage = () => {
     const [expanded, setExpanded] = React.useState(false);
