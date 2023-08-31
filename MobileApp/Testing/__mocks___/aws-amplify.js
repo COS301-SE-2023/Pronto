cc51293f28e6ca8a14ad4282541970392b983dc5
u21598267- J. Albuquerque <u21598267@tuks.co.za>
2023-08-31 16:15:48 +0200

    Tests added
    - Testing for the register page has been added
    - Mock of async storage has been removed from the mocked amplify file
    - Data test IDs to be added for the Register testing

--- MobileApp/Testing/__mocks___/aws-amplify.js
+++ MobileApp/Testing/__mocks___/aws-amplify.js
@@ -1,16 +1,18 @@
 // __mocks__/aws-amplify.js
-// __mocks__/aws-amplify.js
-import MockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
 
-// Mock AsyncStorage
-jest.mock('@react-native-async-storage/async-storage', () => ({
-    __esModule: true,
-    default: MockAsyncStorage,
-}));
 
 const Auth = {
     confirmSignUp: jest.fn(),
     resendSignUp: jest.fn(),
+    signIn: jest.fn(),
+    signUp: jest.fn(),
+    signOut: jest.fn(),
+    currentAuthenticatedUser: jest.fn(),
+    forgotPassword: jest.fn(),
+    forgotPasswordSubmit: jest.fn(),
+    currentSession: jest.fn(),
+    verifyCurrentUserAttribute: jest.fn(),
+    verifyCurrentUserAttributeSubmit: jest.fn(),
 };
 
 module.exports = {
