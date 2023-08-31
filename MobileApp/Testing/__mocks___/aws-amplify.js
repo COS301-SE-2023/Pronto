a1e115c16fc36f96b1b6fe023dbef9ca8d55d43e
u21598267- J. Albuquerque <u21598267@tuks.co.za>
2023-08-31 15:52:17 +0200

    Tests Added
    - Tests for the PasswordMessageCriteria has been added.
    - The test IDs for the tests will be added next for the tests to run successfully
    - A mock file to mock the amplify import has been added for future tests that will require it.
    - AsyncStorage has also needed to be mocked in order for the tests to run with this new mocked file.
    - The package for the AsyncStorage has been added.

--- MobileApp/Testing/__mocks___/aws-amplify.js
+++ MobileApp/Testing/__mocks___/aws-amplify.js
@@ -1,4 +1,12 @@
 // __mocks__/aws-amplify.js
+// __mocks__/aws-amplify.js
+import MockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
+
+// Mock AsyncStorage
+jest.mock('@react-native-async-storage/async-storage', () => ({
+    __esModule: true,
+    default: MockAsyncStorage,
+}));
 
 const Auth = {
     confirmSignUp: jest.fn(),
