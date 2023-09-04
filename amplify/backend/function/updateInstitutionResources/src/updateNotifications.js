ac0e6c5063dcc9290459778849b8bb9f40c44abe
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-04 18:28:43 +0200

    implemented createModuleSegmentCommandInput helper function

--- amplify/backend/function/updateInstitutionResources/src/updateNotifications.js
+++ amplify/backend/function/updateInstitutionResources/src/updateNotifications.js
@@ -1,8 +1,14 @@
 const {
   getAndSetInstitutionDetails,
 } = require("../../pronto5f713f59PreSignup/src/assertInstitutionInfo");
-const { DATASTREAM_ACTIONS, CAMPAIGN_NAME_SUFFIX } = require("./constants");
+const {
+  DATASTREAM_ACTIONS,
+  CAMPAIGN_NAME_SUFFIX,
+  SEGMENT_NAME_SUFFIX,
+  PINPOINT_SEGMENT_DIMENSIONS,
+} = require("./constants");
 
+const PINPOINT_APP_ID = process.env.PINPOINT_APP_ID;
 /*on inst creation:
     build campaign name
     create campaign, if does not exist
@@ -13,6 +19,38 @@ const { DATASTREAM_ACTIONS, CAMPAIGN_NAME_SUFFIX } = require("./constants");
     create module segment
     update compain
 */
+const createModuleSegmentName = (institutionName, moduleCode) => {
+  formattedInstitutionName = institutionName.toLowerCase().replaceAll(" ", "+");
+  formattedModuleCode = moduleCode.toLowerCase().replaceAll(" ", "+");
+  return (
+    formattedInstitutionName + ":" + formattedModuleCode + SEGMENT_NAME_SUFFIX
+  );
+};
+
+const createModuleSegmentCommandInput = (institutionName, moduleCode) => {
+  const moduleSegmentName = createModuleSegmentName(
+    institutionName,
+    moduleCode
+  );
+  const segmentDimensions = {
+    Attributes: {
+      Values: moduleCode,
+      AttributeType: PINPOINT_SEGMENT_DIMENSIONS.ATTRIBUTES.ATTRIBUTE_TYPE,
+    },
+    Behavior: PINPOINT_SEGMENT_DIMENSIONS.BEHAVIOUR,
+    Demographic: PINPOINT_SEGMENT_DIMENSIONS.DEMOGRAPHIC,
+  };
+  const writeSegmentRequest = {
+    Name: moduleSegmentName,
+    Dimensions: segmentDimensions,
+  };
+  const createSegmentCommandInput = {
+    ApplicationId: PINPOINT_APP_ID,
+    WriteSegmentRequest: writeSegmentRequest,
+  };
+  return createSegmentCommandInput;
+};
+
 const createCampaignNames = (institutionName) => {
   formattedInstitutionName = institutionName.toLowerCase().replaceAll(" ", "+");
   emailCampaignName = formattedInstitutionName + CAMPAIGN_NAME_SUFFIX.EMAIL;
@@ -29,18 +67,27 @@ const createCampaignNames = (institutionName) => {
 const createPinpointCampaign = async (institutionName) => {
   const campaignNames = createCampaignNames(institutionName);
   const createEmailCampaignCommandInput = {
-    ApplicationId: process.env.PINPOINT_APP_ID,
-    WriteCampaignRequest: {},
-  };
-  const createSmsCampaignCommandInput = {
-    ApplicationId: process.env.PINPOINT_APP_ID,
+    ApplicationId: PINPOINT_APP_ID,
     WriteCampaignRequest: {
+      Name: campaignNames.emailCampaignName,
+      Description: `${institutionName} EMAIL Notifications Campaign`,
+    },
+  };
+
+  const createSmsCampaignCommandInput = {
+    ApplicationId: PINPOINT_APP_ID,
+    WriteCampaignRequest: {
+      Name: campaignNames.smsCampaignName,
       Description: `${institutionName} SMS Notifications Campaign`,
     },
   };
+
   const createPushCampaignCommandInput = {
-    ApplicationId: process.env.PINPOINT_APP_ID,
-    WriteCampaignRequest: {},
+    ApplicationId: PINPOINT_APP_ID,
+    WriteCampaignRequest: {
+      Name: campaignNames.pushCampaignName,
+      Description: `${institutionName} PUSH Notifications Campaign`,
+    },
   };
 };
 
@@ -58,4 +105,8 @@ const updateNotifications = async (UpdateOption) => {
   }
 };
 
-module.exports = { createCampaignNames };
+module.exports = {
+  createCampaignNames,
+  createModuleSegmentName,
+  createModuleSegmentCommandInput,
+};
