e5f879b2ec41dc42bd1301268fe6d5dc49e2083a
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-05 10:11:18 +0200

    updated tests to reflect new logic

--- amplify/backend/test/unit/function/updateInstitutionResources/updateNotifications.test.js
+++ amplify/backend/test/unit/function/updateInstitutionResources/updateNotifications.test.js
@@ -1,11 +1,9 @@
 const {
   createCampaignNames,
-  createModuleSegmentName,
-  createModuleSegmentCommandInput,
+  createPinpointCampaignCommandInput,
 } = require("../../../../function/updateInstitutionResources/src/updateNotifications");
 
 const institutionName = "University OF Pretoria";
-const moduleCode = "COS301";
 
 describe("Testing helper functions", () => {
   test("testing createCampaignNames", () => {
@@ -13,14 +11,27 @@ describe("Testing helper functions", () => {
       .toLowerCase()
       .replaceAll(" ", "+");
 
-    const expectedCampaignNames = {
-      emailCampaignName:
-        formattedInstitutionName + ":email:notifications:Campaign",
-      smsCampaignName: formattedInstitutionName + ":sms:notifications:Campaign",
-      pushCampaignName:
-        formattedInstitutionName + ":push:notifications:Campaign",
+    const expectedCampaignName =
+      formattedInstitutionName + ":notifications:campaign";
+    const receivedCampaignName = createCampaignNames(institutionName);
+    expect(receivedCampaignName).toEqual(expectedCampaignName);
+  });
+
+  test("testing createPinpointCampaignCommandInput", () => {
+    const campaignNames = createCampaignNames(institutionName);
+    const expectedCampaignCommandInput = {
+      WriteCampaignRequest: {
+        Name: campaignNames.emailCampaignName,
+        Description: `${institutionName} Notifications Campaign`,
+        Schedule: {
+          StartTime: "IMMEDIATE",
+        },
+        TemplateConfiguration: {
+          EmailTemplate: { Name: process.env.EMAIL_TEMPLATE_NAME },
+          PushTemplate: { Name: process.env.PUSH_TEMPLATE_NAME },
+          SMSTemplate: { Name: process.env.SMS_TEMPLATE_NAME },
+        },
+      },
     };
-    const receivedCampaignNames = createCampaignNames(institutionName);
-    expect(receivedCampaignNames).toEqual(expectedCampaignNames);
   });
 });
