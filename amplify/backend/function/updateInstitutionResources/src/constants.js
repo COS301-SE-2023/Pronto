036c72d6555e2143ba09c3ecb8fec28a1bfca710
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-05 09:21:08 +0200

    moved create segment module

--- amplify/backend/function/updateInstitutionResources/src/constants.js
+++ amplify/backend/function/updateInstitutionResources/src/constants.js
@@ -20,46 +20,8 @@ const CAMPAIGN_NAME_SUFFIX = Object.freeze({
   PUSH_NOTIFICATIONS: ":push:notifications:Campaign",
 });
 
-const SEGMENT_NAME_SUFFIX = ":notifications:segment";
-
-const PINPOINT_SEGMENT_DIMENSIONS = Object.freeze({
-  ATTRIBUTES: { ATTRIBUTE_TYPE: "INCLUSIVE" },
-  BEHAVIOUR: {
-    Recency: {
-      Duration: "DAY_30",
-      RecencyType: "ACTIVE",
-    },
-  },
-  EMAIL_DEMOGRAPHIC: {
-    Channel: {
-      Values: ["EMAIL"],
-      DimensionType: "INCLUSIVE",
-    },
-  },
-  PUSH_DEMOGRAPHIC: {
-    Channel: {
-      Values: ["PUSH"],
-      DimensionType: "INCLUSIVE",
-    },
-  },
-  SMS_DEMOGRAPHIC: {
-    Channel: {
-      Values: ["SMS"],
-      DimensionType: "INCLUSIVE",
-    },
-  },
-  SEGMENT_GROUPS: {
-    STUDENT_GROUP: {
-      TYPE: "ANY",
-    },
-    INCLUDE: "ANY",
-  },
-});
-
 module.exports = {
   DYNAMODB_CONSTANTS,
   DATASTREAM_ACTIONS,
   CAMPAIGN_NAME_SUFFIX,
-  SEGMENT_NAME_SUFFIX,
-  PINPOINT_SEGMENT_DIMENSIONS,
 };
--- amplify/backend/function/updateInstitutionResources/src/updateNotifications.js
+++ amplify/backend/function/updateInstitutionResources/src/updateNotifications.js
@@ -1,12 +1,7 @@
 const {
   getAndSetInstitutionDetails,
 } = require("../../pronto5f713f59PreSignup/src/assertInstitutionInfo");
-const {
-  DATASTREAM_ACTIONS,
-  CAMPAIGN_NAME_SUFFIX,
-  SEGMENT_NAME_SUFFIX,
-  PINPOINT_SEGMENT_DIMENSIONS,
-} = require("./constants");
+const { DATASTREAM_ACTIONS, CAMPAIGN_NAME_SUFFIX } = require("./constants");
 
 const PINPOINT_APP_ID = process.env.PINPOINT_APP_ID;
 
@@ -20,64 +15,6 @@ const PINPOINT_APP_ID = process.env.PINPOINT_APP_ID;
     create module segment
     update compain
 */
-const createModuleSegmentName = (institutionName, moduleCode) => {
-  formattedInstitutionName = institutionName.toLowerCase().replaceAll(" ", "+");
-  formattedModuleCode = moduleCode.toLowerCase().replaceAll(" ", "+");
-  return (
-    formattedInstitutionName + ":" + formattedModuleCode + SEGMENT_NAME_SUFFIX
-  );
-};
-
-const createModuleSegmentCommandInput = (institutionName, moduleCode) => {
-  const moduleSegmentName = createModuleSegmentName(
-    institutionName,
-    moduleCode
-  );
-  const emailSegmentGroupDimensions = {
-    Attributes: {
-      Values: [moduleCode],
-      AttributeType: PINPOINT_SEGMENT_DIMENSIONS.ATTRIBUTES.ATTRIBUTE_TYPE,
-    },
-    Behavior: PINPOINT_SEGMENT_DIMENSIONS.BEHAVIOUR,
-    Demographic: PINPOINT_SEGMENT_DIMENSIONS.EMAIL_DEMOGRAPHIC,
-  };
-  const smsSegmentGroupDimensions = {
-    Attributes: {
-      Values: [moduleCode],
-      AttributeType: PINPOINT_SEGMENT_DIMENSIONS.ATTRIBUTES.ATTRIBUTE_TYPE,
-    },
-    Behavior: PINPOINT_SEGMENT_DIMENSIONS.BEHAVIOUR,
-    Demographic: PINPOINT_SEGMENT_DIMENSIONS.SMS_DEMOGRAPHIC,
-  };
-  const pushSegmentGroupDimensions = {
-    Attributes: {
-      Values: [moduleCode],
-      AttributeType: PINPOINT_SEGMENT_DIMENSIONS.ATTRIBUTES.ATTRIBUTE_TYPE,
-    },
-    Behavior: PINPOINT_SEGMENT_DIMENSIONS.BEHAVIOUR,
-    Demographic: PINPOINT_SEGMENT_DIMENSIONS.PUSH_DEMOGRAPHIC,
-  };
-  const studentsSegmentGroup = {
-    Dimensions: [
-      emailSegmentGroupDimensions,
-      smsSegmentGroupDimensions,
-      pushSegmentGroupDimensions,
-    ],
-    Type: PINPOINT_SEGMENT_DIMENSIONS.SEGMENT_GROUPS.STUDENT_GROUP.TYPE,
-  };
-  const writeSegmentRequest = {
-    Name: moduleSegmentName,
-    SegmentGroups: {
-      Groups: [studentsSegmentGroup],
-      Include: PINPOINT_SEGMENT_DIMENSIONS.SEGMENT_GROUPS.INCLUDE,
-    },
-  };
-  const createSegmentCommandInput = {
-    ApplicationId: PINPOINT_APP_ID,
-    WriteSegmentRequest: writeSegmentRequest,
-  };
-  return createSegmentCommandInput;
-};
 
 const createCampaignNames = (institutionName) => {
   formattedInstitutionName = institutionName.toLowerCase().replaceAll(" ", "+");
@@ -139,8 +76,6 @@ const updateNotifications = async (UpdateOption) => {
 };
 
 module.exports = {
-  createModuleSegmentName,
-  createModuleSegmentCommandInput,
   createCampaignNames,
   createPinpointCampaignCommandInput,
 };
