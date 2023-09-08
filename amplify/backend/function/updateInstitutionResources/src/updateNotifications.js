0a363c0a217c6af27b1499597cfdfea8528d8584
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-08 09:45:10 +0200

    added onInstitude deleted handler

--- amplify/backend/function/updateInstitutionResources/src/updateNotifications.js
+++ amplify/backend/function/updateInstitutionResources/src/updateNotifications.js
@@ -1,6 +1,4 @@
-const {
-  getAndSetInstitutionDetails,
-} = require("../../pronto5f713f59PreSignup/src/assertInstitutionInfo");
+const { CreateCampaignCommand } = require("@aws-sdk/client-pinpoint");
 const { DATASTREAM_ACTIONS, CAMPAIGN_NAME_SUFFIX } = require("./constants");
 
 const PINPOINT_APP_ID = process.env.PINPOINT_APP_ID;
@@ -30,15 +28,65 @@ const createPinpointCampaignCommandInput = (institutionName) => {
   return createCampaignCommandInput;
 };
 
-const updateInstitudeResources = async (UpdateOption) => {
+const putCampainIdOnInstitution = async (institutionId, campaignId) => {
+  const updateInstitutionMutation = /* GraphQL */ `
+    query updateInstitution($input: UpdateInstitutionInput!) {
+      getInstitution(id: $input) {
+        name
+        adminId
+        domains
+        lectureremails
+      }
+    }
+  `;
+  const variables = {
+    input: institution.id,
+  };
+  const options = {
+    method: "POST",
+    headers: {
+      "x-api-key": GRAPHQL_API_KEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ updateInstitutionMutation, variables }),
+  };
+
+  const request = new Request(GRAPHQL_ENDPOINT, options);
+  let body;
+  let response;
+
+  try {
+    response = await fetch(request);
+  } catch (getEmailsQueryError) {}
+};
+
+const updateInstitudeResources = async (UpdateOption, pinpointClient) => {
   switch (UpdateOption) {
     case DATASTREAM_ACTIONS.INSTITUDE_CREATED:
       //CREATE campain
       //write campainID to institutionDB
       //Update notifications status on institution table -> send test email?
+      const campaignCommandInput =
+        createPinpointCampaignCommandInput(institutionName);
+      const createCampaignCommand = new CreateCampaignCommand(
+        campaignCommandInput
+      );
+      try {
+        const createCampainResponse = await pinpointClient.send(
+          createCampaignCommand
+        );
+      } catch (error) {}
+      break;
+    case DATASTREAM_ACTIONS.INSTITUDE_UPDATED:
+      try {
+      } catch (error) {}
+      break;
+    case DATASTREAM_ACTIONS.INSTITUDE_DELETED:
+      try {
+      } catch (error) {}
       break;
     default:
-      break;
+      throw new Error();
   }
 };
 
