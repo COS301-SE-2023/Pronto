270c5b611d685f61da105ea997cd36f009f8d8e9
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-14 08:07:23 +0200

    new configs

--- amplify/backend/jest.config.js
+++ amplify/backend/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   setupFiles: ["<rootDir>/test/.jest/setEnvVars.js"],
   collectCoverage: true,
   coverageReporters: ["json", "html"],
-  coverageDirectory: "<rootDir>test/coverage",
+  coverageDirectory: "<rootDir>/test/coverage",
 };
