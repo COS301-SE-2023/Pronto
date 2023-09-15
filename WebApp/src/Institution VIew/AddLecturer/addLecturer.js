71acc4260498753766b46e78451e8429e1a8af0c
Tymac15 <u21578878@tuks.co.za>
2023-09-15 20:01:20 +0200

    Update addLecturer.js
    cleaned up add lecturer imports

--- WebApp/src/Institution VIew/AddLecturer/addLecturer.js
+++ WebApp/src/Institution VIew/AddLecturer/addLecturer.js
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
+
 import InstitutionNavigation from "../Navigation/InstitutionNavigation";
 import { createLecturer, deleteLecturer, updateCourse, updateInstitution } from "../../Graphql/mutations";
 import { lecturersByInstitutionId, searchLecturers, listAdmins, searchLecturerByCourses } from "../../Graphql/queries";
-
-import { API, Auth } from 'aws-amplify';
 import AddModal from './addCourse';
 import { ErrorModal } from "../../Components/ErrorModal";
-import SearchSharpIcon from '@mui/icons-material/SearchSharp';
-import ClearIcon from '@mui/icons-material/Clear';
 import HelpButton from '../../Components/HelpButton';
 import UserManual from "../HelpFiles/AddLecturer.pdf";
 import { useAdmin } from "../../ContextProviders/AdminContext";
 import { useLecturerList } from "../../ContextProviders/LecturerListContext";
+
+import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import ClearIcon from '@mui/icons-material/Clear';
+
+import { API, Auth } from 'aws-amplify';
 
 const AddLecturer = () => {
 
