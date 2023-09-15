f8eda3c232428a03d82b74abef800b88aa8d7c6b
u20612894 <u20612894@tuks.co.za>
2023-09-15 08:59:39 +0200

    Load map
    Loading map now happens in Post Accordion instead of index

--- WebApp/src/index.js
+++ WebApp/src/index.js
@@ -6,17 +6,21 @@ import reportWebVitals from './reportWebVitals';
 import {LoadScript} from '@react-google-maps/api';
 
 
-const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API;
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <LoadScript googleMapsApiKey={googleMapsApiKey} libraries={["places"]}>
-            <App/>
-        </LoadScript>
-    </React.StrictMode>
-);
+console.log("starting");
+// const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API;
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+//     <React.StrictMode>
+        // <LoadScript googleMapsApiKey={googleMapsApiKey} libraries={["places"]}>
+//             <App/>
+//         </LoadScript>
+//</React.StrictMode>
+      <React.StrictMode>
+        <App/>
+      </React.StrictMode>
+ );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ reportWebVitals();
