54e20ff644b7e865b09a4c709164cc5ac84c4030
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-15 10:55:40 +0200

    restoring preauth busines logic

--- amplify/backend/function/prontoPostSignUp/src/index.js
+++ amplify/backend/function/prontoPostSignUp/src/index.js
@@ -1,21 +1,33 @@
-/* Amplify Params - DO NOT EDIT
-	AUTH_PRONTOAUTH_USERPOOLID
-	ENV
-	REGION
-Amplify Params - DO NOT EDIT */
+/**
+ * @fileoverview
+ *
+ * This CloudFormation Trigger creates a handler which awaits the other handlers
+ * specified in the `MODULES` env var, located at `./${MODULE}`.
+ */
 
 /**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ * The names of modules to load are stored as a comma-delimited string in the
+ * `MODULES` env var.
  */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+const moduleNames = process.env.MODULES.split(',');
+/**
+ * The array of imported modules.
+ */
+const modules = moduleNames.map((name) => require(`./${name}`));
+
+/**
+ * This async handler iterates over the given modules and awaits them.
+ *
+ * @see https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html#nodejs-handler-async
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ *
+ */
+exports.handler = async (event, context) => {
+  /**
+   * Instead of naively iterating over all handlers, run them concurrently with
+   * `await Promise.all(...)`. This would otherwise just be determined by the
+   * order of names in the `MODULES` var.
+   */
+  await Promise.all(modules.map((module) => module.handler(event, context)));
+  return event;
 };
