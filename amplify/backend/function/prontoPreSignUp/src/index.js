920925ce4580aaaadd5aa31c5b3c05ccaad6433b
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-15 11:30:28 +0200

    added all source tp appropiate cf

--- amplify/backend/function/prontoPreSignUp/src/index.js
+++ amplify/backend/function/prontoPreSignUp/src/index.js
@@ -1,23 +1,75 @@
 /* Amplify Params - DO NOT EDIT
-	API_PRONTOGRAPHQLAPI_GRAPHQLAPIENDPOINTOUTPUT
-	API_PRONTOGRAPHQLAPI_GRAPHQLAPIIDOUTPUT
-	API_PRONTOGRAPHQLAPI_GRAPHQLAPIKEYOUTPUT
 	ENV
 	REGION
+	API_PRONTO_GRAPHQLAPIIDOUTPUT
+	API_PRONTO_GRAPHQLAPIENDPOINTOUTPUT
+	API_PRONTO_GRAPHQLAPIKEYOUTPUT
+	AUTH_PRONTOAUTH_USERPOOLID
+	StudentsGroupName
+	LecturersGroupName
+	AdminGroupName
 Amplify Params - DO NOT EDIT */
-
+const ROLES = require("./roles.js");
+const {
+  isLectureEmailPartOfInstitution,
+  isAdminAllocated,
+  isStudentEmailDomainPartOfInstitution,
+} = require("./assertInstitutionInfo.js");
+const isAppClientValid = require("./isAppClientValid.js");
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+  console.table(event);
+  if (!event.request.clientMetadata.role)
+    throw new Error("User role not provided on ClientMetadata");
+  if (
+    !isAppClientValid(
+      event.callerContext.clientId,
+      event.request.clientMetadata.role
+    )
+  )
+    throw new Error(
+      `Cannot authenticate user from this app client: 
+      Students Should use the mobile app and Admin/Lectures should use the web app`
+    );
+
+  event.response.autoConfirmUser = false;
+  try {
+    switch (event.request.clientMetadata.role) {
+      case ROLES.Admin:
+        if (
+          await isAdminAllocated(event.request.clientMetadata.institutionId)
+        ) {
+          throw new Error(`Institution has an admin already.`);
+        }
+        break;
+      case ROLES.Lecture:
+        const isLectureEmailPartOfInst = await isLectureEmailPartOfInstitution(
+          event.request.userAttributes.email,
+          event.request.clientMetadata.institutionId
+        );
+
+        if (!isLectureEmailPartOfInst) {
+          throw new Error(`Lecturer email is not part of the Institution.`);
+        }
+        break;
+      case ROLES.Student:
+        if (
+          !await isStudentEmailDomainPartOfInstitution(
+            event.request.userAttributes.email,
+            event.request.clientMetadata.institutionId
+          )
+        )
+          throw new Error(
+            `The provided student email does not match the selected institutions student emails format.
+          Please use your institution provided email.`
+          );
+        break;
+    }
+  } catch (preAuthError) {
+    console.debug(preAuthError);
+    throw new Error(preAuthError);
+  }
+  return event;
 };
