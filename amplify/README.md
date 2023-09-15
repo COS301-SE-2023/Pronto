bd407e77bfa35ff30088ded875a989de762fe070
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-15 08:44:47 +0200

    added auth

--- amplify/README.md
+++ amplify/README.md
@@ -13,3 +13,31 @@
 ### Testing
 
 ### License
+
+# Installation
+
+# Usage
+
+# Configuration
+
+## Required libraries
+
+`node v18.17.0` and `amplify cli v12.4.0`
+
+1. intitialize existing Pronto project
+
+`amplify init --app https://github.com/COS301-SE-2023/Pronto `
+
+2. Authenticate AWS Profile
+   use one of the provided options by the cli to sign-in to an aws account
+
+# Architecture
+
+# Testing
+
+## Set-Up
+
+1. `jenv v0.5.6` -- to install java, optional
+2. `java v16.0.1`
+
+# License
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "prontoAuth": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "adminUserGroupGroupRole": "string",
+      "lecturerUserGroupGroupRole": "string",
+      "studentUserGroupGroupRole": "string"
+    }
+  }
+}
