edbb0a9d6db9d52582ed3931da058826382f9845
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-16 10:19:35 +0200

    updating unit tests

---
+++ amplify/backend/test/unit/function/prontoPreSignIn/index.test.js
@@ -0,0 +1,4 @@
+const adminEvent = require("../../../../function/prontoPreSignUp/src/events/admin.event.json");
+const lecturerEvent = require("../../../../function/prontoPreSignUp/src/events/lecturers.event.json");
+const studentsEvent = require("../../../../function/prontoPreSignUp/src/events/students.event.json");
+const preAuth = require("../../../../function/prontoPreSignUp/src/index");
