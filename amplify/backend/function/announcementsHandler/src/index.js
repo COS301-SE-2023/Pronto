c72fbade082c6fb2035a717b67e098d3b40fef91
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-17 19:02:17 +0200

    added responses

--- amplify/backend/function/announcementsHandler/src/index.js
+++ amplify/backend/function/announcementsHandler/src/index.js
@@ -9,7 +9,8 @@ Amplify Params - DO NOT EDIT */
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-const { GRAPHQL } = require("./helpers/constants");
+const { GRAPHQL, NOTIFICATIONS_STATUS } = require("./helpers/constants");
+const { sendMessageOperations } = require("./helpers/notificationsService");
 
 const config = {
   region: process.env.AWS_REGION,
@@ -28,16 +29,36 @@ exports.handler = async (event) => {
   if (graphQlObject === GRAPHQL.OBJECT_TYPES.MUTATION) {
     switch (fieldName) {
       case GRAPHQL.FIELD_TYPES.CREATE_ANNOUNCEMENT:
-        return { SMS: "", EMAIL: "", PUSH: "" };
+        const sendMessageOperationInput = {
+          course: course,
+          announcement: announcement,
+          pinpointClient,
+        };
+        return await sendMessageOperations(sendMessageOperationInput);
 
       case GRAPHQL.FIELD_TYPES.UPDATE_ANNOUNCEMENT:
-        return { SMS: "", EMAIL: "", PUSH: "" };
+        return {
+          SMS: NOTIFICATIONS_STATUS.DISABLED,
+          EMAIL: NOTIFICATIONS_STATUS.DISABLED,
+          PUSH: NOTIFICATIONS_STATUS.DISABLED,
+        };
       case GRAPHQL.FIELD_TYPES.DELETE_ANNOUNCEMENT:
-        return { SMS: "", EMAIL: "", PUSH: "" };
-
+        return {
+          SMS: NOTIFICATIONS_STATUS.DISABLED,
+          EMAIL: NOTIFICATIONS_STATUS.DISABLED,
+          PUSH: NOTIFICATIONS_STATUS.DISABLED,
+        };
       default:
-        return { SMS: "", EMAIL: "", PUSH: "" };
+        return {
+          SMS: NOTIFICATIONS_STATUS.FAILED,
+          EMAIL: NOTIFICATIONS_STATUS.FAILED,
+          PUSH: NOTIFICATIONS_STATUS.FAILED,
+        };
     }
   }
-  return { SMS: "", EMAIL: "", PUSH: "" };
+  return {
+    SMS: NOTIFICATIONS_STATUS.FAILED,
+    EMAIL: NOTIFICATIONS_STATUS.FAILED,
+    PUSH: NOTIFICATIONS_STATUS.FAILED,
+  };
 };
