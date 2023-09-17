ced2aa1da26a0298b7f3a15fd546c5f0fb177a52
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-17 10:08:01 +0200

    added setAndGetNotificationStatus

---
+++ amplify/backend/function/announcementsHandler/src/index.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+
+const setAndGetNotificationStatus = (response) => {
+  return {
+    RequestId: response.RequestId,
+    STATUS: response.STATUS,
+  };
+};
+
+exports.handler = async (event) => {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  return {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+};
