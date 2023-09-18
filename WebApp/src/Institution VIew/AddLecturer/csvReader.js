bd9659dfb59168f8721d1c3c1c9867230b391f06
u20612894 <u20612894@tuks.co.za>
2023-09-18 07:22:40 +0200

    Parse headers
    Removing underscores and white spaces

--- WebApp/src/Institution VIew/AddLecturer/csvReader.js
+++ WebApp/src/Institution VIew/AddLecturer/csvReader.js
@@ -18,96 +18,111 @@ const CsvFileReader = (props)=>{
         
   };
 
+    const papaparseOptions = {
+    header: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+    transformHeader: header =>
+      header
+        .toLowerCase()
+        .replace(" ","")
+        .replace("_","")
+
+  }
+
   const addLecturers = async(lecturerList)=>{
     props.setAdding("Adding...This may take a while");
     let emailList=props.emailList;
-
+    console.log(lecturerList);
     for(let i=0;i<lecturerList.length;i++){
         try{
           
             let lecturer={
                 institutionId:props.institutionId,
-                firstname:lecturerList[i]['First Name'] ? lecturerList[i]['First Name'] : lecturerList[i]['FirstName']? lecturerList[i]['FirstName'] : lecturerList[i]['Firstname'] ?lecturerList[i]['Firstname']:lecturerList[i]['First name']? lecturerList[i]['First name'] : lecturerList[i]['Name']? lecturerList[i]['Name'] : lecturerList[i]['name']?  lecturerList[i]['name'] : undefined,
-                lastname:lecturerList[i]['Last Name'] ? lecturerList[i]['Last Name'] : lecturerList[i]['LastName']? lecturerList[i]['LastName'] : lecturerList[i]['Lastname'] ?lecturerList[i]['Lastname']:lecturerList[i]['Last name']? lecturerList[i]['Last name'] : lecturerList[i]['Surname']? lecturerList[i]['Surname'] : lecturerList[i]['surname']?  lecturerList[i]['surname'] : undefined,
-                email:lecturerList[i]['Email Address'] ? lecturerList[i]['Email Address'] : lecturerList[i]['Email']? lecturerList[i]['Email'] : lecturerList[i]['email address']? lecturerList[i]['email address'] : lecturerList[i]['email']? lecturerList[i]['email'] : undefined,
+                firstname:lecturerList[i]['firstname'] ? lecturerList[i]['firstname']  : lecturerList[i]['name']?  lecturerList[i]['name'] : undefined,
+                lastname: lecturerList[i]['lastname']? lecturerList[i]['lastname']  : lecturerList[i]['surname']?  lecturerList[i]['surname'] : undefined,
+                email:lecturerList[i]['emailaddress'] ? lecturerList[i]['emailaddress'] : lecturerList[i]['email']? lecturerList[i]['email'] : undefined,
                 userRole:"Lecturer"
             }
             if(lecturer.firstname===undefined || lecturer.lastname===undefined || lecturer.email===undefined){
                 if(i>0){
+                  console.log(lecturer);
                   continue;
                 }
                 else{
+                  console.log(lecturer);
                   props.setError("File contains unrecognised fields.Please use First Name,Last Name, Email Address and Courses");
                   break;
                 }
             }else{
-              if(props.adminEmail!==lecturer.email){
-                let emails = await API.graphql({
-                        query: listLecturers,
-                        variables: { filter: { email: { eq: lecturer.email } } }
-                    })
-                if(emails.data.listLecturers.items.length===0){
-                  let newLecturer=await API.graphql({
-                        query:createLecturer,
-                        variables:{input:lecturer}
-                      })
+              // if(props.adminEmail!==lecturer.email){
+              //   let emails = await API.graphql({
+              //           query: listLecturers,
+              //           variables: { filter: { email: { eq: lecturer.email } } }
+              //       })
+              //   if(emails.data.listLecturers.items.length===0){
+              //     let newLecturer=await API.graphql({
+              //           query:createLecturer,
+              //           variables:{input:lecturer}
+              //         })
                   
-                  newLecturer=newLecturer.data.createLecturer;
-                  emailList.push(lecturer.email);
-                  let courses=lecturerList[i]['Courses']? lecturerList[i]['Courses'].split(',') : lecturerList[i]['courses']? lecturerList[i]['courses'].split(',') : lecturerList[i]['Course']?  lecturerList[i]['Course'].split(',') : lecturerList[i]['course']? lecturerList[i]['course'].split(',') : []
-                  let filter = `{"filter" : { "or" : [`;
-                  console.log("courses here");
-                  console.log(courses);
-                  if(courses.length>0){
-                      for (let i = 0; i < courses.length; i++) {
-                        if(courses[i]!=="" && courses[i]!==" " ){
-                          if (i === courses.length - 1) {
-                            filter += `{"coursecode":{"eq":"${courses[i]}" } }`;
-                          }
-                        else {
-                          filter += `{"coursecode":{"eq":"${courses[i]}" } },`;
-                        } 
-                      }
-                    }
-                    filter += `] }}`;
+              //     newLecturer=newLecturer.data.createLecturer;
+              //     emailList.push(lecturer.email);
+              //     let courses=lecturerList[i]['courses']? lecturerList[i]['courses'].split(',') : lecturerList[i]['course']?  lecturerList[i]['course'].split(',') : []
+              //     let filter = `{"filter" : { "or" : [`;
+              //     console.log("courses here");
+              //     console.log(courses);
+              //     if(courses.length>0){
+              //         for (let i = 0; i < courses.length; i++) {
+              //           if(courses[i]!=="" && courses[i]!==" " ){
+              //             if (i === courses.length - 1) {
+              //               filter += `{"coursecode":{"eq":"${courses[i]}" } }`;
+              //             }
+              //           else {
+              //             filter += `{"coursecode":{"eq":"${courses[i]}" } },`;
+              //           } 
+              //         }
+              //       }
+              //       filter += `] }}`;
                  
-                    let variables = JSON.parse(filter);
-                    console.log(variables);
+              //       let variables = JSON.parse(filter);
+              //       console.log(variables);
                     
-                    try{
-                      let courseList=await API.graphql({
-                      query:listCourses,
-                      variables:variables
-                    })
-                    // let list=await API.graphql({
-                    //   query:listCourses,
-                    //   variables:{}
-                    // })
-                    console.log(courseList);
-                    courseList=courseList.data.listCourses.items;
-                    for(let i=0;i<courseList.length;i++){
-                      console.log("adding courses");  
-                      let a=await API.graphql({
-                          query:updateCourse,
-                          variables:{input:{id:courseList[i].id,lecturerId:newLecturer.id}}
-                        })
-                         console.log(a);
-                    }
+              //       try{
+              //         let courseList=await API.graphql({
+              //         query:listCourses,
+              //         variables:variables
+              //       })
+              //       // let list=await API.graphql({
+              //       //   query:listCourses,
+              //       //   variables:{}
+              //       // })
+              //       console.log(courseList);
+              //       courseList=courseList.data.listCourses.items;
+              //       for(let i=0;i<courseList.length;i++){
+              //         console.log("adding courses");  
+              //         let a=await API.graphql({
+              //             query:updateCourse,
+              //             variables:{input:{id:courseList[i].id,lecturerId:newLecturer.id}}
+              //           })
+              //            console.log(a);
+              //       }
                   
-                  }catch(e){
-                    console.log(e) 
-                  }
-                }
-                }
-              }
-          }
+                //   }catch(e){
+                //     console.log(e) 
+                //   }
+                // }
+                console.log("ok");
+                 }
+            //  }
+          //}
         }catch(error){
             console.log(error);
         }
-        let ins=API.graphql({
-          query:updateInstitution,
-          variables:{input:{id:props.institutionId,lectureremails:emailList}}
-        })
+        // let ins=API.graphql({
+        //   query:updateInstitution,
+        //   variables:{input:{id:props.institutionId,lectureremails:emailList}}
+        // })
       
     }
     props.setAdding("Add")
@@ -115,11 +130,10 @@ const CsvFileReader = (props)=>{
 
     return (
       <div>
-        <h1>Add via CSV</h1>
         <CSVReader
           onFileLoaded={handleFile}
-          inputStyle={{ color: 'red' }} 
-          parserOptions={{ header: true, dynamicTyping: true }}
+          inputStyle={{ color: "#e32f45" }} 
+          parserOptions={papaparseOptions}
         />
       </div>
     );
