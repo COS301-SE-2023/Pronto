b861f6a6675456e799b67b2925ae49e8ed3e8b31
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-19 08:53:46 +0200

    new handler scaffoulding

---
+++ amplify/backend/function/notificationPreferanceHandler/src/index.js
@@ -0,0 +1,23 @@
+/* Amplify Params - DO NOT EDIT
+	API_PRONTOGRAPHQLAPI_GRAPHQLAPIENDPOINTOUTPUT
+	API_PRONTOGRAPHQLAPI_GRAPHQLAPIIDOUTPUT
+	API_PRONTOGRAPHQLAPI_GRAPHQLAPIKEYOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+};
