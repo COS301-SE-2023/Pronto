6805f301748011bbf155e754ccfa420f51a1cf8c
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-19 18:16:56 +0200

    EMAIL TEMPLATE

---
+++ amplify/backend/function/announcementsHandler/src/helpers/message.js
@@ -0,0 +1,28 @@
+const createSubject = (announcement) => {
+  return {
+    Charset: "UTF-8",
+    Data: ``,
+  };
+};
+
+const createHtmlPart = (announcement) => {
+  const body = ``;
+  return {
+    Charset: "UTF-8",
+    Data: body,
+  };
+};
+
+const createTextPart = (announcement) => {
+  const body = ``;
+  return {
+    Charset: "UTF-8",
+    Data: body,
+  };
+};
+
+module.exports = {
+  createSubject,
+  createHtmlPart,
+  createTextPart,
+};
