26eb357875e9417be5e7a34a3e5ddef55d3a533f
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-19 18:18:52 +0200

    removed cov configs

--- amplify/backend/jest.config.js
+++ amplify/backend/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   setupFiles: ["<rootDir>/test/.jest/setEnvVars.js"],
   collectCoverage: true,
-  coverageReporters: ["json", "html"],
   coverageDirectory: "<rootDir>/test/coverage",
 };
