076ee6a2ebc6553fd74109a8aa4cee67b537d5a7
u21598267- J. Albuquerque <u21598267@tuks.co.za>
2023-09-20 08:09:42 +0200

    New Method For Hiding Test Data
    - We need to hide the credentials used for the integration tests and thus the following method has been used.
    ### Method
    - Create a repo secret
    - Call the repo secret in the testing file

--- WebApp/test/e2e/cypress/e2e/auth/signIn.spec.cy.js
+++ WebApp/test/e2e/cypress/e2e/auth/signIn.spec.cy.js
@@ -1,13 +1,12 @@
 describe("Testing sign In", () => {
   // Load the environment variable for users.json data
-  const usersJson = Cypress.env('E2E_USERS_JSON');
+  const lecturerPassword= Cypress.env('LECTURER_PASSWORD');
 
   // Check if the environment variable is set
-  if (!usersJson) {
-    throw new Error("E2E_USERS_JSON environment variable is not set.");
+  if (!lecturerPassword) {
+    throw new Error("The lecturers password environment variable is not set.");
   }
 
-  const users = JSON.parse(usersJson);
 
   /* The test below is responsible for signing in a student, this test uses the incorrect credentials
      and should result in a failed sign in
@@ -25,7 +24,7 @@ describe("Testing sign In", () => {
   it("should sign in lecturer", function () {
     cy.LecturerSignIn(
         users.lecturers[1].email,
-        users.lecturers[1].password
+        lecturerPassword
     );
   });
 });
