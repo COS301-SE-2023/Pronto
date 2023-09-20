5bcfeb1b6ea5d262901b3e85f95349803c436b77
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-09-20 17:26:02 +0200

    Changed mobile view to component
    Changed mobile view to component

--- WebApp/src/Homepage/HomePage.js
+++ WebApp/src/Homepage/HomePage.js
@@ -9,6 +9,7 @@ import ScrollDownIcon from './Images/scroll-down.png';
 import ProntoLogo from "../Images/ProntoLogo.svg";
 import AboutImage from "./Images/AboutPicture.png";
 import DesktopAccess from "./Images/DesktopAccess.png";
+import MobileView from "./MobileView";
 
 function HomePage() {
   const [scrollingUp, setScrollingUp] = useState(true); // Track scroll direction
@@ -67,18 +68,7 @@ function HomePage() {
       {/* Conditional rendering to show/hide the Navbar */}
       {isMobileView ? (
         // Display a message for mobile users
-        <div className="mobile-message">
-          <img src={ProntoLogo} alt="ProntoLogo" className='logoMobile' />
-          <h4>Welcome to Pronto!</h4>
-          <br />
-          <h3>Students</h3>
-          <p>Students can download ExpoGo from the <a href="https://itunes.apple.com/app/apple-store/id982107779">App store</a> on IOS, or get it from <a href="https://play.google.com/store/apps/details?id=host.exp.exponent&referrer=www">Google Play</a> if you are on Android, to use Pronto</p>
-          <img className='mobileAboutImage' src={AboutImage} alt="mobileAboutImage" />
-          < br />
-          <h4>Lecturer or admin</h4>
-          <p>If you are a lecturer or admin, please access the website from a desktop.</p>
-          <img src={DesktopAccess} alt="ProntoLogo" className='mobileImages' />
-        </div>
+        < MobileView />
       ) : (
         // Content for desktop users
         <div>
