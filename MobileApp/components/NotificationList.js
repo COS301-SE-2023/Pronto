f2b47edccc9edbfc3938d0bc62b3e0686213ea30
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-09-20 18:03:56 +0200

    Added mock data for announcements that can be used
    Added mock data for announcements that can be used

--- MobileApp/components/NotificationList.js
+++ MobileApp/components/NotificationList.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Alert, View, StyleSheet,Modal, Text ,RefreshControl,IconButton,Pressable} from "react-native";
-import { List, Card, Avatar,Button,Portal,PaperProvider } from "react-native-paper";
+import { Alert, View, StyleSheet, Modal, Text, RefreshControl, IconButton, Pressable } from "react-native";
+import { List, Card, Avatar, Button, Portal, PaperProvider } from "react-native-paper";
 import { ScrollView } from "react-native";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
-import { listStudents,getStudent,announcementsByDate, listAnnouncements } from "../graphql/queries";
+import { listStudents, getStudent, announcementsByDate, listAnnouncements } from "../graphql/queries";
 import { Auth, API } from "aws-amplify"
 import { useStudent } from "../ContextProviders/StudentContext";
 import { useAnnouncement } from "../ContextProviders/AnnouncementContext";
@@ -11,221 +11,243 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const NotificationList = ({ navigation }) => {
 
+  //if you need to use mock data for styling
+  const mockAnnouncements = require('../assets/data/mock/mock-announcement.json');
+
   const [expanded1, setExpanded1] = useState(false);
   const [expanded2, setExpanded2] = useState(false);
-  const {student,updateStudent} =useStudent();
-  const [selectedAnnouncement,setSelectedAnnouncement]=useState(null);
-  const [isModalVisible,setIsModalVisible]=useState(false);
-  const {announcement,setAnnouncement,nextToken,setNextToken}=useAnnouncement();
+  const { student, updateStudent } = useStudent();
+  const [selectedAnnouncement, setSelectedAnnouncement] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const { announcement, setAnnouncement, nextToken, setNextToken } = useAnnouncement();
 
- 
+
   const handlePress1 = () => setExpanded1(!expanded1);
   const handlePress2 = () => setExpanded2(!expanded2);
   const [loading, setLoading] = useState(false);
   const error = "There appear to be network issues. Please try again later";
-  let limit=4;
+  let limit = 4;
   const showFullMessage = (key) => {
     setSelectedAnnouncement(key);
     //setIsModalVisible(true);
     //Alert.alert(key.body);
   };
 
+
+  //mock data to fetch
   const fetchAnnouncements = async () => {
-   
     try {
-      let stu=student;
-      if(student===null){
-        
-        setLoading(true);
-        const user = await Auth.currentAuthenticatedUser()
-        let studentEmail = user.attributes.email;
+      setLoading(true);
 
-        let stu = await API.graphql({
-          query: getStudent,
-          variables: {id:user.attributes.sub}
-        })
-      
-        stu=stu.data.getStudent;
-                
-        if(stu===null || stu===undefined){
-          throw Error();
-        }
-        updateStudent(stu);
+      // Use the mockAnnouncements array as the source of announcements
+      setAnnouncement(mockAnnouncements);
+      setNextToken(null);
 
-      };
-
-      if(announcement.length===0){
-       
-        setLoading(true);
-        let courses=[];
-        for(let i=0;i<stu.enrollments.items.length;i++){
-          courses.push(stu.enrollments.items[i].courseId);
-        }
-         
-        let filter=`{"filter" : { "or" : [`;
-        for(let i=0;i<courses.length;i++){
-          if(i===courses.length-1){
-            filter+=`{"courseId":{"eq":"${courses[i]}" } }`;
-          }
-          else{
-            filter+=`{"courseId":{"eq":"${courses[i]}" } },`;
-          }
-        }
-     
-        filter+=`] },"limit":"${limit}" ,"sortDirection":"DESC"}`;
-        
-        let variables = JSON.parse(filter)
-    
-        let announcementList=await API.graphql({
-            query:listAnnouncements,
-            variables:variables
-          })
-          ;
-        
-        setAnnouncement(announcementList.data.listAnnouncements.items);
-        if(announcementList.data.listAnnouncements.items.length<limit){
-          setNextToken(null)
-        }
-        else{
-          setNextToken(announcementList.data.listAnnouncements.nextToken);
-        }
-      }
       setLoading(false);
-    } catch (er) {
-      
-      Alert.alert(error)
+    } catch (error) {
+      Alert.alert(error);
       setLoading(false);
     }
-  }
+  };
 
-  const onRefresh = async()=>{
-      try{
-        setLoading(true);
+  //normal fetch announcement (NOT MOCK DATA)
+  /*  const fetchAnnouncements = async () => {
+     
+      try {
         let stu=student;
-        let courses=[];
-        for(let i=0;i<stu.enrollments.items.length;i++){
-          courses.push(stu.enrollments.items[i].courseId);
-        }
+        if(student===null){
+          
+          setLoading(true);
+          const user = await Auth.currentAuthenticatedUser()
+          let studentEmail = user.attributes.email;
+  
+          let stu = await API.graphql({
+            query: getStudent,
+            variables: {id:user.attributes.sub}
+          })
+        
+          stu=stu.data.getStudent;
+                  
+          if(stu===null || stu===undefined){
+            throw Error();
+          }
+          updateStudent(stu);
+  
+        };
+  
+        if(announcement.length===0){
          
-        let filter=`{"filter" : { "or" : [`;
-        for(let i=0;i<courses.length;i++){
-          if(i===courses.length-1){
-            filter+=`{"courseId":{"eq":"${courses[i]}" } }`;
+          setLoading(true);
+          let courses=[];
+          for(let i=0;i<stu.enrollments.items.length;i++){
+            courses.push(stu.enrollments.items[i].courseId);
+          }
+           
+          let filter=`{"filter" : { "or" : [`;
+          for(let i=0;i<courses.length;i++){
+            if(i===courses.length-1){
+              filter+=`{"courseId":{"eq":"${courses[i]}" } }`;
+            }
+            else{
+              filter+=`{"courseId":{"eq":"${courses[i]}" } },`;
+            }
+          }
+       
+          filter+=`] },"limit":"${limit}" ,"sortDirection":"DESC"}`;
+          
+          let variables = JSON.parse(filter)
+      
+          let announcementList=await API.graphql({
+              query:listAnnouncements,
+              variables:variables
+            })
+            ;
+          
+          setAnnouncement(announcementList.data.listAnnouncements.items);
+          if(announcementList.data.listAnnouncements.items.length<limit){
+            setNextToken(null)
           }
           else{
-            filter+=`{"courseId":{"eq":"${courses[i]}" } },`;
+            setNextToken(announcementList.data.listAnnouncements.nextToken);
           }
         }
-     
-        filter+=`] },"limit":"${limit}" ,"sortDirection":"DESC"}`;
-        
-        let variables = JSON.parse(filter)
-    
-        let announcementList=await API.graphql({
-            query:listAnnouncements,
-            variables:variables
-          })
-          ;
-        setAnnouncement(announcementList.data.listAnnouncements.items);
-        if(announcementList.data.listAnnouncements.items.length<limit){
-          setNextToken(null);
-        }
-        else{
-          setNextToken(announcementList.data.listAnnouncements.nextToken);
-        }
         setLoading(false);
-      }catch(e){
+      } catch (er) {
         
         Alert.alert(error)
+        setLoading(false);
       }
+    } */
+
+
+  const onRefresh = async () => {
+    try {
+      setLoading(true);
+      let stu = student;
+      let courses = [];
+      for (let i = 0; i < stu.enrollments.items.length; i++) {
+        courses.push(stu.enrollments.items[i].courseId);
+      }
+
+      let filter = `{"filter" : { "or" : [`;
+      for (let i = 0; i < courses.length; i++) {
+        if (i === courses.length - 1) {
+          filter += `{"courseId":{"eq":"${courses[i]}" } }`;
+        }
+        else {
+          filter += `{"courseId":{"eq":"${courses[i]}" } },`;
+        }
+      }
+
+      filter += `] },"limit":"${limit}" ,"sortDirection":"DESC"}`;
+
+      let variables = JSON.parse(filter)
+
+      let announcementList = await API.graphql({
+        query: listAnnouncements,
+        variables: variables
+      })
+        ;
+      setAnnouncement(announcementList.data.listAnnouncements.items);
+      if (announcementList.data.listAnnouncements.items.length < limit) {
+        setNextToken(null);
+      }
+      else {
+        setNextToken(announcementList.data.listAnnouncements.nextToken);
+      }
+      setLoading(false);
+    } catch (e) {
+
+      Alert.alert(error)
+    }
 
   }
 
-  const loadMore =async()=>{
-    try{
+  const loadMore = async () => {
+    try {
 
-      let stu=student;
-      let year=new Date().getFullYear();
-      let courses=[];
-        
-      for(let i=0;i<stu.enrollments.items.length;i++){
+      let stu = student;
+      let year = new Date().getFullYear();
+      let courses = [];
+
+      for (let i = 0; i < stu.enrollments.items.length; i++) {
         courses.push(stu.enrollments.items[i].courseId);
       }
-      
-      let filter=`{"filter" : { "or" : [`;
-      for(let i=0;i<courses.length;i++){
-        if(i===courses.length-1){
-          filter+=`{"courseId":{"eq":"${courses[i]}" } }`;
+
+      let filter = `{"filter" : { "or" : [`;
+      for (let i = 0; i < courses.length; i++) {
+        if (i === courses.length - 1) {
+          filter += `{"courseId":{"eq":"${courses[i]}" } }`;
         }
-        else{
-          filter+=`{"courseId":{"eq":"${courses[i]}" } },`;
+        else {
+          filter += `{"courseId":{"eq":"${courses[i]}" } },`;
         }
       }
-        
-      filter+=`] },"limit":"${limit}","sortDirection":"DESC","nextToken":"${nextToken}"}`;  
+
+      filter += `] },"limit":"${limit}","sortDirection":"DESC","nextToken":"${nextToken}"}`;
       let variables = JSON.parse(filter)
-         
-      let announcementList=await API.graphql({
-            query:listAnnouncements,
-            variables:variables
-          });
-        
-      let a=announcementList.data.listAnnouncements.items;
-      for(let i=0;i<a.length;i++){
+
+      let announcementList = await API.graphql({
+        query: listAnnouncements,
+        variables: variables
+      });
+
+      let a = announcementList.data.listAnnouncements.items;
+      for (let i = 0; i < a.length; i++) {
         announcement.push(a[i]);
       }
-      if(announcementList.data.listAnnouncements.items.length<limit){
+      if (announcementList.data.listAnnouncements.items.length < limit) {
         setNextToken(null);
       }
-      else{
+      else {
         setNextToken(announcementList.data.listAnnouncements.nextToken);
       }
       setAnnouncement(announcement)
-    }catch(e){
-     
+    } catch (e) {
+
       Alert.alert(error);
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchAnnouncements();
-  },[])
+  }, [])
 
   return (
     <View >
 
-   { selectedAnnouncement && 
-      <Modal
-        animationType="slide"
-        transparent={true}
-        onRequestClose={() => {
-      
-          setSelectedAnnouncement(null)
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.headerStyle}>{selectedAnnouncement.title}</Text>
-            <Text style={styles.subheaderStyle}>{selectedAnnouncement.course.coursecode}</Text>
-            <Text style={styles.textStyle}>{selectedAnnouncement.body}</Text>
-         <Button
-            icon="thumb-up"
-            mode="contained"
-            style={{
-              backgroundColor: "#e32f45",
-              width: "70%",
-              margin: "5%",
-              textAlign: "center",
-              color: "white",
-            }}
+      {selectedAnnouncement &&
+        <Modal
+          animationType="slide"
+          transparent={true}
+          onRequestClose={() => {
 
-            outlined={true}
-            onPress={() => setSelectedAnnouncement(null)}
-          >
-            Ok
-            </Button>
+            setSelectedAnnouncement(null)
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.headerStyle}>{selectedAnnouncement.title}</Text>
+              <Text style={styles.subheaderStyle}>{selectedAnnouncement.course.coursecode}</Text>
+              <Text style={styles.textStyle}>{selectedAnnouncement.body}</Text>
+              <Button
+                icon="thumb-up"
+                mode="contained"
+                style={{
+                  backgroundColor: "#e32f45",
+                  width: "70%",
+                  margin: "5%",
+                  textAlign: "center",
+                  color: "white",
+                }}
+
+                outlined={true}
+                onPress={() => setSelectedAnnouncement(null)}
+              >
+                Ok
+              </Button>
+            </View>
           </View>
-        </View>
-      </Modal>
+        </Modal>
       }
       <List.Section title="Announcements" style={{ margin: 10 }}>
         <List.Accordion
@@ -238,7 +260,7 @@ const NotificationList = ({ navigation }) => {
           onPress={handlePress1}
           style={{ backgroundColor: "white" }}
         >
-          {announcement.filter(item=>item.type==="Reminder").map((val, key) => (
+          {announcement.filter(item => item.type === "Reminder").map((val, key) => (
             <Card
               key={key}
               style={{
@@ -268,7 +290,7 @@ const NotificationList = ({ navigation }) => {
           onPress={handlePress2}
           style={{ backgroundColor: "white" }}
         >
-          {announcement.filter(item=>item.type==="Due Assignment").map((val, key) => (
+          {announcement.filter(item => item.type === "Due Assignment").map((val, key) => (
             <Card
               key={key}
               style={{
@@ -285,10 +307,10 @@ const NotificationList = ({ navigation }) => {
               </Card.Content>
             </Card>
           ))}
-       
+
         </List.Accordion>
-      </List.Section> 
-       
+      </List.Section>
+
       <Card.Content>
         <List.Section title="Recent Announcements">
           <View>
@@ -311,15 +333,15 @@ const NotificationList = ({ navigation }) => {
                 }}
               >No recent announcements</Text>
             ) : (
-              < ScrollView 
+              < ScrollView
                 style={{ height: "70%" }}
-                 refreshControl={
-                    <RefreshControl
-                      refreshing={loading}
-                      onRefresh={onRefresh}
+                refreshControl={
+                  <RefreshControl
+                    refreshing={loading}
+                    onRefresh={onRefresh}
                   />
-                 } 
-                >
+                }
+              >
                 {announcement.map((val, key) => (
                   <Card
                     key={key}
@@ -338,7 +360,7 @@ const NotificationList = ({ navigation }) => {
                         left={(props) => (
                           <Avatar.Icon
                             {...props}
-                            icon={val.type==="Reminder"? "brain" : "clock"}
+                            icon={val.type === "Reminder" ? "brain" : "clock"}
                             color="#e32f45"
                             style={{ backgroundColor: "white" }}
                           />
@@ -347,44 +369,44 @@ const NotificationList = ({ navigation }) => {
                     </Card.Content>
                   </Card>
                 ))}
-        <Text
-          style={{
-            marginBottom:"0%",
-            marginLeft:"auto",
-            marginRight:"auto"
-          }}
-          >
-          { nextToken !==null ? 
-              <Button 
-                onPress={loadMore} 
-                mode="contained"
-                icon="arrow-down"
-                outlined={true}
-                testID="load-more-button"
-                style={{
-                  backgroundColor: "#e32f45",
-                  marginRight:"auto",
-                  marginLeft:"auto",
-                  color:"white"
-                }}
-              > 
-                  Load More 
-                </Button> 
-              :  
-              " "
-            }
-        </Text>
-        <Text style={{marginLeft:"auto",marginRight:"auto"}}>Swipe down to refresh &#x2193;</Text>
-        </ScrollView>
-              
+                <Text
+                  style={{
+                    marginBottom: "0%",
+                    marginLeft: "auto",
+                    marginRight: "auto"
+                  }}
+                >
+                  {nextToken !== null ?
+                    <Button
+                      onPress={loadMore}
+                      mode="contained"
+                      icon="arrow-down"
+                      outlined={true}
+                      testID="load-more-button"
+                      style={{
+                        backgroundColor: "#e32f45",
+                        marginRight: "auto",
+                        marginLeft: "auto",
+                        color: "white"
+                      }}
+                    >
+                      Load More
+                    </Button>
+                    :
+                    " "
+                  }
+                </Text>
+                <Text style={{ marginLeft: "auto", marginRight: "auto" }}>Swipe down to refresh &#x2193;</Text>
+              </ScrollView>
+
             )}
-            
+
           </View>
-          
+
         </List.Section>
-       
+
       </Card.Content>
-      </View>
+    </View>
   );
 };
 
@@ -411,7 +433,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
     marginBottom: 100,
     width: "80%",
-    height:"50%",
+    height: "50%",
     paddingBottom: "0%"
   },
   button: {
@@ -423,7 +445,7 @@ const styles = StyleSheet.create({
     fontSize: 15,
     textAlign: "center",
     marginBottom: "5%",
- },
+  },
   headerStyle: {
     fontSize: 30,
     fontWeight: "bold",
