068b4f2816d494325af4d483bfc20de79ab4d841
u21598267- J. Albuquerque <u21598267@tuks.co.za>
2023-09-20 11:30:19 +0200

    Fixed Directories
    - due to a reverted commit we lost some directories that were changed during a code cleanup

--- WebApp/src/Error pages/NotFound.js
+++ WebApp/src/Error pages/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import "./error.css";
-import ProntoSvg from '../images/ProntoLogo.svg';
+import ProntoSvg from '../Images/ProntoLogo.svg';
 
 function NotFound() {
     const navigate = useNavigate();
