f08c693b0bfc36fd83f4f99a199362ea01810133
Tymac15 <105007195+Tymac15@users.noreply.github.com>
2023-09-20 12:52:11 +0200

    Update message.js
    Checked and placed correct variable according to template and query parameters

--- amplify/backend/function/announcementsHandler/src/helpers/message.js
+++ amplify/backend/function/announcementsHandler/src/helpers/message.js
@@ -218,7 +218,7 @@ const createHtmlPart = (announcement) => {
     <td class="es-container-visible-simple-html-only" valign="top" align="center" style="padding:0;Margin:0;width:560px">
     <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
     <tr>
-    <td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:27px;color:#000000;font-size:18px"><strong>${announcement.course.coursecode}&nbsp;New Announcement: ${announcement.title}</strong></p></td>
+    <td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:27px;color:#000000;font-size:18px"><strong>${announcement.course.coursecode} New ${announcement.type}</strong></p></td>
     </tr>
     </table></td>
     </tr>
