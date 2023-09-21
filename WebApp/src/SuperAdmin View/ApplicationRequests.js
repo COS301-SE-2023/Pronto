3d4c44bb79add341db180e921756315c6d279578
ShashinGounden <101881243+ShashinGounden@users.noreply.github.com>
2023-09-21 20:19:47 +0200

    Finalised super admin pages
    Finalised super admin pages

--- WebApp/src/SuperAdmin View/ApplicationRequests.js
+++ WebApp/src/SuperAdmin View/ApplicationRequests.js
@@ -1,50 +1,64 @@
 import React, { useState } from 'react';
 import SuperAdminNavigation from './SuperAdminNavigation';
-
-// Import Material Icons
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
 
 export default function ApplicationRequests() {
-  // Define the initial list of application requests
   const [requests, setRequests] = useState([
     'John Doe (john@example.com) requested to be an administrator for University A',
     'Jane Smith (jane@example.com) requested to be an administrator for University B',
   ]);
 
-  // Function to accept a request
   const acceptRequest = (index) => {
-    // Update the requests list by removing the accepted request
     const updatedRequests = [...requests];
     updatedRequests.splice(index, 1);
     setRequests(updatedRequests);
   };
 
-  // Function to decline a request
   const declineRequest = (index) => {
-    // Update the requests list by removing the declined request
     const updatedRequests = [...requests];
     updatedRequests.splice(index, 1);
     setRequests(updatedRequests);
   };
 
   return (
-    <div style={{ textAlign: 'center', height: '100vh' }}>
-      <h1 style={{ paddingTop: '20%' }}>Application Requests</h1>
-      <div style={{ display: "flex", flexDirection: "column" }}>
+    <div style={{ display: 'flex', maxHeight: '100vh' }}>
+      <nav style={{ width: '20%' }}>
+        <SuperAdminNavigation />
+      </nav>
+
+      <main style={{ width: '900px', marginTop: '30px' }}>
+        <h1 className="moduleHead" style={{ textShadow: '2px 2px 4px rgba(0, 0.3, 0.2, 0.3)' }}>
+          Application Requests
+        </h1>
+
         {requests.map((request, index) => (
-          <div style={{ display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "20px", borderRadius: "20px", padding: '10px', width: '100%', boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.3)", height: "80px" }} key={index}>
-            <div>
-              <p>{request}</p>
+          <div className="card" key={index}>
+            <div className="card-header">
+              <div className="subjectCode">Application #{index + 1}</div>
+              <div className="postDate">Date Applied</div>
             </div>
-            <div style={{ marginLeft: "20px" }}>
-              <CheckIcon onClick={() => acceptRequest(index)} style={{ cursor: 'pointer', color: 'green', marginRight: '10px' }} />
-              <CloseIcon onClick={() => declineRequest(index)} style={{ cursor: 'pointer', color: 'red' }} />
+            <div className="card-body">
+              <h5 className="card-title">University Name</h5>
+              <p className="card-text">{request}</p>
+
+              <div style={{ float: "right" }}>
+
+
+                <CheckIcon
+                  onClick={() => acceptRequest(index)}
+                  style={{ cursor: 'pointer', color: 'green', marginRight: '10px' }}
+                />
+                <CloseIcon
+                  onClick={() => declineRequest(index)}
+                  style={{ cursor: 'pointer', color: 'red' }}
+                />
+
+              </div>
             </div>
           </div>
         ))}
-      </div>
-      <SuperAdminNavigation />
-    </div >
+      </main>
+    </div>
   );
 }
--- WebApp/src/SuperAdmin View/ViewInstitutions.js
+++ WebApp/src/SuperAdmin View/ViewInstitutions.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import SuperAdminNavigation from './SuperAdminNavigation';
+import Button from '@mui/material/Button';
 
 export default function ViewInstitutions() {
+    // Define an array of mock institutions
+    const institutions = [
+        {
+            name: 'Tuks',
+            adminEmail: 'example@example.com',
+            dateApplied: '2023-09-21',
+        },
+        {
+            name: 'University B',
+            adminEmail: 'admin@example.com',
+            dateApplied: '2023-09-20',
+        },
+    ];
+
     return (
-        <div>
-            <SuperAdminNavigation />
-            View Institutions
-        </div >
+        <div style={{ display: 'flex', maxHeight: '100vh' }}>
+            <nav style={{ width: '20%' }}>
+                {/* Navigation bar content */}
+                <SuperAdminNavigation />
+            </nav>
+
+            <main style={{ width: '900px', marginTop: '30px' }}>
+                <h1 className="moduleHead" style={{ textShadow: '2px 2px 4px rgba(0, 0.3, 0.2, 0.3)' }}>
+                    View Institutions
+                </h1>
+
+                {institutions.map((institution, index) => (
+                    <div className="card" key={index}>
+                        <div className="card-header">
+                            <div className="subjectCode">Institution Application</div>
+                            <div className="postDate">{institution.dateApplied}</div>
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title">Institution Name: {institution.name}</h5>
+                            <p className="card-text">Admin Email Address: {institution.adminEmail}</p>
+
+                            <Button
+                                aria-haspopup="true"
+                                variant="contained"
+                                disableElevation
+                                style={{ float: 'right', backgroundColor: '#e32f45', borderRadius: '20px' }}
+                            >
+                                Delete
+                            </Button>
+                        </div>
+                    </div>
+                ))}
+            </main>
+        </div>
     );
 }
