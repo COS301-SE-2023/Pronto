1e4dbbc377a4abe9594a5740f13eabed3f76a617
u20612894 <u20612894@tuks.co.za>
2023-09-22 14:37:32 +0200

    Requests
    Requests can now be accepted or deleted  Accepting a request changes the administration email to the requesters email and marks the request as "ACCEPTED". Declining a request marks the request as "REJECTED". Only request marked "PENDING" are shown so the request is removed from the view either way

--- WebApp/src/SuperAdmin View/ApplicationRequests.js
+++ WebApp/src/SuperAdmin View/ApplicationRequests.js
@@ -2,24 +2,78 @@ import React, { useState } from 'react';
 import SuperAdminNavigation from './SuperAdminNavigation';
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
+import { useEffect } from 'react';
+import {API} from "aws-amplify";
+import { getInstitution ,listAdminApplications} from '../Graphql/queries';
+import { updateAdmin,updateAdminApplication } from '../Graphql/mutations';
 
 export default function ApplicationRequests() {
-  const [requests, setRequests] = useState([
-    'John Doe (john@example.com) requested to be an administrator for University A',
-    'Jane Smith (jane@example.com) requested to be an administrator for University B',
-  ]);
+  
+  const [requests,setRequests]=useState([]);
+  
 
-  const acceptRequest = (index) => {
+  const acceptRequest = async(index) => {
     const updatedRequests = [...requests];
-    updatedRequests.splice(index, 1);
-    setRequests(updatedRequests);
+    
+    try{
+       let admin=await API.graphql({
+        query:getInstitution,
+        variables:{id:requests[index].firstname}
+       })
+       admin=admin.data.getInstitution.admin;
+       admin.email=requests[index].email;
+       let updated=await API.graphql({
+        query:updateAdmin,
+        variables:{input:admin}
+       })
+          let updateRequest=await API.graphql({
+            query:updateAdminApplication,
+            variables:{input:{id:requests[index].id,status:"ACCEPTED"}}
+          })
+        updatedRequests.splice(index, 1);
+        setRequests(updatedRequests);  
+    }catch(error){
+      console.log(error);
+    }
   };
 
-  const declineRequest = (index) => {
+  const declineRequest = async(index) => {
     const updatedRequests = [...requests];
-    updatedRequests.splice(index, 1);
-    setRequests(updatedRequests);
+    try{
+      let updateRequest=await API.graphql({
+            query:updateAdminApplication,
+            variables:{input:{id:requests[index].id,status:"REJECTED"}}
+          })
+      
+      updatedRequests.splice(index, 1);
+      setRequests(updatedRequests);
+    }catch(error){
+      console.log(error)
+    }
   };
+
+  const fetchRequests=async()=>{
+    try{
+      let r=await API.graphql({
+        query:listAdminApplications,
+        variables:{
+          filter:{
+            status:{
+              eq:"PENDING"
+            }
+          }
+        }
+      })
+      setRequests(r.data.listAdminApplications.items);
+    
+    }catch(error){
+      console.log(error)
+    }
+
+  }
+  useEffect(()=>{
+    fetchRequests();
+  },[])
 
   return (
     <div style={{ display: 'flex', maxHeight: '100vh' }}>
@@ -36,12 +90,13 @@ export default function ApplicationRequests() {
           <div className="card" key={index}>
             <div className="card-header">
               <div className="subjectCode">Application #{index + 1}</div>
-              <div className="postDate">Date Applied</div>
+              <div className="postDate">{request.createdAt.split("T")[0]}</div>
             </div>
             <div className="card-body">
-              <h5 className="card-title">University Name</h5>
-              <p className="card-text">{request}</p>
-
+              <h5 className="card-title">{request.lastname}</h5>
+              <p className="card-text">{request.name+ " applied to be an admin"}</p>
+               <p className='card-text'>Contact them at <a href="mailto:">{request.email}</a></p>
+            
               <div style={{ float: "right" }}>
 
 
