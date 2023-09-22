4c9e523d610414655e6c74c2d74cd8b9ee41dba1
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-22 10:42:15 +0200

    validate email operation

--- amplify/backend/function/notificationPreferanceHandler/src/helpers/smsService.js
+++ amplify/backend/function/notificationPreferanceHandler/src/helpers/smsService.js
@@ -6,3 +6,29 @@ const isPhoneNumberPatternValid = (phoneNumber) => {
   return mobilePhoneNumberPattern.test(phoneNumber);
 };
 
+const validateMobilePhoneNumberOperation = async (
+  validateMobilePhoneNumberOperation
+) => {
+  const phoneNumberValidateCommandInput = {
+    NumberValidateRequest: {
+      IsoCountryCode: validateMobilePhoneNumberOperation.IsoCountryCode,
+      PhoneNumber: validateMobilePhoneNumberOperation.phoneNumber,
+    },
+  };
+  const phoneNumberValidateCommand = new PhoneNumberValidateCommand(
+    phoneNumberValidateCommandInput
+  );
+  const pinpointClient = validateMobilePhoneNumberOperation.pinpointClient;
+  try {
+    const phoneNumberValidateCommandOutput = await pinpointClient.send(
+      phoneNumberValidateCommand
+    );
+    const { $metadata, NumberValidateResponse } =
+      phoneNumberValidateCommandOutput;
+    return { NumberValidateResponse: NumberValidateResponse, error: null };
+  } catch (phoneNumberValidateError) {
+    console.debug(`ERROR VALIDATING PHONE NUMBER: ${phoneNumberValidateError}`);
+    throw new Error("failed to verify phone number");
+  }
+};
+
