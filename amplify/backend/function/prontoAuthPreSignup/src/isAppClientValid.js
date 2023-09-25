3306c1c8fc09900ab8e74ddbfdd69a12f36aaf52
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-25 09:17:48 +0200

    moved preSignup to new backend

--- amplify/backend/function/prontoAuthPreSignup/src/isAppClientValid.js
+++ amplify/backend/function/prontoAuthPreSignup/src/isAppClientValid.js
@@ -3,9 +3,9 @@ function isAppClientValid(clientId, role) {
   if (!Object.values(ROLES).includes(role))
     throw new Error("Invalid User Role");
   switch (clientId) {
-    case process.env.COGNITO_WEB_CLIENT_ID:
+    case process.env.AppClientIdWeb:
       return ROLES.Lecture === role || ROLES.Admin === role;
-    case process.env.COGNITO_MOBILE_CLIENT_ID:
+    case process.env.AppClientId:
       return ROLES.Student === role;
     default:
       throw new Error(`Unrecognised user pool app client ID`);
