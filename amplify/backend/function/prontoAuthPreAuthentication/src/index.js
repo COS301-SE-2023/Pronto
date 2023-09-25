08aee011426640b59ee2017a8e3bf7eeeda6ead8
bandisa <89698022+Bandisa@users.noreply.github.com>
2023-09-25 08:54:54 +0200

    moved index file

--- amplify/backend/function/prontoAuthPreAuthentication/src/index.js
+++ amplify/backend/function/prontoAuthPreAuthentication/src/index.js
@@ -1,75 +1,50 @@
 /* Amplify Params - DO NOT EDIT
+	API_PRONTOGRAPHQLAPI_GRAPHQLAPIENDPOINTOUTPUT
+	API_PRONTOGRAPHQLAPI_GRAPHQLAPIIDOUTPUT
+	API_PRONTOGRAPHQLAPI_GRAPHQLAPIKEYOUTPUT
 	ENV
 	REGION
-	API_PRONTO_GRAPHQLAPIIDOUTPUT
-	API_PRONTO_GRAPHQLAPIENDPOINTOUTPUT
-	API_PRONTO_GRAPHQLAPIKEYOUTPUT
-	AUTH_PRONTOAUTH_USERPOOLID
-	StudentsGroupName
-	LecturersGroupName
-	AdminGroupName
 Amplify Params - DO NOT EDIT */
-const ROLES = require("./roles.js");
+const ROLES = require("./roles");
 const {
-  isLectureEmailPartOfInstitution,
-  isAdminAllocated,
-  isStudentEmailDomainPartOfInstitution,
-} = require("./assertInstitutionInfo.js");
-const isAppClientValid = require("./isAppClientValid.js");
+  isInstitideAdminOrLecturer,
+} = require("./helpers/assertInstitutionInfo");
+const isAppClientValid = require("./helpers/isAppClientValid");
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
 exports.handler = async (event) => {
   console.table(event);
   if (
-    !event.request.clientMetadata.role ||
-    !Object.values(ROLES).includes(event.request.clientMetadata.role)
+    !event.request.validationData.role ||
+    !Object.values(ROLES).includes(event.request.validationData.role)
   )
     throw new Error("Invalid User Role or Role not provided");
   if (
     !isAppClientValid(
       event.callerContext.clientId,
-      event.request.clientMetadata.role
+      event.request.validationData.role
     )
   )
     throw new Error(
       `Cannot authenticate user from this app client: 
       Students Should use the mobile app and Admin/Lectures should use the web app`
     );
-
-  event.response.autoConfirmUser = false;
   try {
-    switch (event.request.clientMetadata.role) {
-      case ROLES.Admin:
-        if (
-          await isAdminAllocated(event.request.clientMetadata.institutionId)
-        ) {
-          throw new Error(`Institution has an admin already.`);
-        }
-        break;
-      case ROLES.Lecture:
-        const isLectureEmailPartOfInst = await isLectureEmailPartOfInstitution(
-          event.request.userAttributes.email,
-          event.request.clientMetadata.institutionId
-        );
-
-        if (!isLectureEmailPartOfInst) {
-          throw new Error(`Lecturer email is not part of the Institution.`);
-        }
-        break;
-      case ROLES.Student:
-        if (
-          !(await isStudentEmailDomainPartOfInstitution(
-            event.request.userAttributes.email,
-            event.request.clientMetadata.institutionId
-          ))
-        )
-          throw new Error(
-            `The provided student email does not match the selected institutions student emails format.
-          Please use your institution provided email.`
-          );
-        break;
-    }
+    if (
+      !(await isInstitideAdminOrLecturer(
+        event.request.userAttributes.email,
+        event.request.validationData.institutionId,
+        event.request.validationData.role
+      )) &&
+      process.env.COGNITO_WEB_CLIENT_ID === event.callerContext.clientId
+    )
+      throw new Error(`Only admins/lecturer are allowed to use the web app\n
+      Please Use the mobile if you are student, or sign up as a lecturer/admin
+      Request for an account as an institude admin\n
+      Or contact your institude admin if you are a lecture\n
+      More details on: {path/to/pronto/web/about/institude/admin}`);
   } catch (preAuthError) {
     console.debug(preAuthError);
     throw new Error(preAuthError);
