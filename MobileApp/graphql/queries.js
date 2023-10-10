be840c602a2d4a702253056886d02c0c9bb3e9db
u20612894 <u20612894@tuks.co.za>
2023-10-10 10:24:07 +0200

    Modify queries

--- MobileApp/graphql/queries.js
+++ MobileApp/graphql/queries.js
@@ -1,322 +1,174 @@
-export const announcementsByCourseId=`query AnnouncementsByCourseId(
-  $courseId: ID!
-  $sortDirection: ModelSortDirection
-  $filter: ModelAnnouncementFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  announcementsByCourseId(
-    courseId: $courseId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
-  ) {
-    items {
-      id
-      body
-      title
-      date
-      venue
-    }
-    nextToken
-  }
-}`
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
 
-
-
-export const activitiesByCourseId=`query ActivitiesByCourseId(
-  $courseId: ID!
-  $sortDirection: ModelSortDirection
-  $filter: ModelActivityFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  activitiesByCourseId(
-    courseId: $courseId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
-  ) {
-    items {
-      id
-      courseId
-      activityname
-      day
-      start
-      end
-      venue
-    }
-    nextToken
-  }
-}
-`
-
-export const searchCourses=`query SearchCourses(
-  $filter: SearchableCourseFilterInput
-  $sort: [SearchableCourseSortInput]
-  $limit: Int
-  $nextToken: String
-  $from: Int
-  $aggregates: [SearchableCourseAggregationInput]
-) {
-  searchCourses(
-    filter: $filter
-    sort: $sort
-    limit: $limit
-    nextToken: $nextToken
-    from: $from
-    aggregates: $aggregates
-  ) {
-    items {
-      id
-      institutionId
-      coursecode
-      activity { 
-        items { 
-          id
-          activityname
-          day
-          start
-          end 
-          venue
-          courseId
-        }
-      }
-    }
-    nextToken
-    total
-    aggregateItems {
-      name
-      result {
-        ... on SearchableAggregateScalarResult {
-          value
-        }
-        ... on SearchableAggregateBucketResult {
-          buckets {
-            key
-            doc_count
-          }
-        }
-      }
-    }
-  }
-}
-`
-
-export const coursesByInstitutionId=`query CoursesByInstitutionId(
-  $institutionId: ID!
-  $sortDirection: ModelSortDirection
-  $filter: ModelCourseFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  coursesByInstitutionId(
-    institutionId: $institutionId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
-  ) {
-    items {
-      id
-      institutionId
-      lecturerId
-      coursecode
-      
-    }
-    nextToken
-  }
-}`
-
-export const listTimetables=`query ListTimetables(
-  $filter: ModelTimetableFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listTimetables(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      studentId
-      activityId
-      createdAt
-      updatedAt
-    }
-    nextToken
-  }
-}`
-
-export const studentsByInstitutionId=`query StudentsByInstitutionId(
-  $institutionId: ID!
-  $sortDirection: ModelSortDirection
-  $filter: ModelStudentFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  studentsByInstitutionId(
-    institutionId: $institutionId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
-  ) {
-    items {
+export const getStudent = /* GraphQL */ `
+  query GetStudent($id: ID!) {
+    getStudent(id: $id) {
       institutionId
       id
       firstname
       lastname
-      userRole
       email
-      studentTimetableId
-    }
-    nextToken
-  }
-}`
-
-export const enrollmentsByStudentId=`query EnrollmentsByStudentId(
-  $studentId: ID!
-  $sortDirection: ModelSortDirection
-  $filter: ModelEnrollmentFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  enrollmentsByStudentId(
-    studentId: $studentId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
-  ) {
-    items {
-      id
-      studentId
-      courseId
-      course { 
-        id 
-        coursecode
-      }
-    }
-    nextToken
-  }
-}`
-
-export const listStudents=`query ListStudents(
-  $filter: ModelStudentFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listStudents(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      institutionId
-      institution{
-        name
-      }
-      id
-      firstname
-      lastname
-      email
-      studentTimetableId
-      timetable { 
+      preference {
+        studentId
+        endpoint {
+          type
+          endPointAddress
+          status
+          info
+          __typename
+        }
         id
-        activities{ 
-           items { 
-              id
-              courseId
-              activityname
-              day
-              start
-              end
-              venue
-              course{
-                coursecode
-              }
-            }
-          }
-        activityId  
-        }
-      enrollments { 
-        items{
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
+      }
+      timetable {
+        id
+        studentId
+        activityId
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        owner
+        __typename
+      }
+      enrollments {
+        items {
           id
+          studentId
           courseId
-          owner
-          course{ 
+          course {
             id
             coursecode
             activity{
               items{
-                courseId
                 activityname
-                coordinates
-                id
                 day
                 start
                 end
-                venue 
+                id
+                venue
+                coordinates
+                _version
+                _deleted
+                _lastChangedAt
                 course{
                   coursecode
                 }
               }
             }
+            createdAt
+            updatedAt
+            _version
+            _deleted
+            _lastChangedAt
+            owner
+            __typename
           }
+          createdAt
+          updatedAt
+          _version
+          _deleted
+          _lastChangedAt
+          owner
+          __typename
         }
-      }  
-    }
-    nextToken
-  }
-}`
-
-export const listInstitutions=`query ListInstitutions(
-  $filter: ModelInstitutionFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listInstitutions(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      name
-      domains
+      }
+      createdAt
+      updatedAt
       _version
       _deleted
+      _lastChangedAt
+      studentTimetableId
+      owner
+      __typename
     }
-    nextToken
   }
-}`
+`;
 
-export const listCourses=`query ListCourses(
-  $filter: ModelCourseFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listCourses(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      institutionId
-      coursecode
-    }
-    nextToken
-  }
-}`
-export const getCourse=`query GetCourse($id: ID!) {
-  getCourse(id: $id) {
-    id
-    institutionId
-    lecturerId
-    coursecode
-    semester
-    enrollments {
+export const listInstitutions = /* GraphQL */ `
+  query ListInstitutions(
+    $filter: ModelInstitutionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listInstitutions(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
+      }
       nextToken
-    }
-    activity {
-      nextToken
-    }
-    announcents {
-      nextToken
+      startedAt
+      __typename
     }
   }
-}
-`
-export const listAnnouncements = `query ListAnnouncements(
+`;
+
+
+export const listCourses = /* GraphQL */ `
+  query ListCourses(
+    $filter: ModelCourseFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listCourses(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        institutionId
+        lecturerId
+        coursecode
+        activity {
+          items {
+            id
+            courseId
+            activityname
+            day
+            start
+            end
+            venue
+            coordinates
+            createdAt
+            updatedAt
+            _version
+            _deleted
+            _lastChangedAt
+            owner
+            __typename
+          }
+          nextToken
+          startedAt
+          __typename
+        }
+       
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        owner
+        __typename
+      }
+      nextToken
+      startedAt
+      __typename
+    }
+  }
+`;
+
+export const listAnnouncements = /* GraphQL */ `
+  query ListAnnouncements(
     $filter: ModelAnnouncementFilterInput
     $limit: Int
     $nextToken: String
@@ -325,102 +177,38 @@ export const listAnnouncements = `query ListAnnouncements(
       items {
         id
         courseId
+        lecturerId
+        description
+        start
+        end
+        date
+        venue
+        course {
+          id
+          coursecode
+          createdAt
+          updatedAt
+          _version
+          _deleted
+          _lastChangedAt
+          __typename
+        }
         body
         title
-        date
-        course{
-          coursecode
-        }
         type
+        year
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
       }
-
       nextToken
+      startedAt
+      __typename
     }
   }
 `;
 
-export const announcementsByDate=`query AnnouncementsByDate ( 
-        $year: String!,  
-        $createdAt: ModelStringKeyConditionInput, 
-        $sortDirection: ModelSortDirection, 
-        $filter: ModelAnnouncementFilterInput, 
-        $limit: Int, 
-        $nextToken: String
-      ){
-          announcementsByDate( 
-            year:$year,  
-            createdAt: $createdAt, 
-            sortDirection: $sortDirection, 
-            filter: $filter, 
-            limit: $limit, 
-            nextToken: $nextToken
-            ){ 
-              items{ 
-                id
-                title
-                body
-                date
-                createdAt
-                course{
-                  coursecode   
-                }
-                type
-              }
-              nextToken
-            }
-}`;
-
-export const getStudent=`query GetStudent($id: ID!) {
-  getStudent(id: $id) {
-      institutionId
-      id
-      firstname
-      lastname
-      email
-      studentTimetableId
-      timetable { 
-        id
-        activities{ 
-           items { 
-              id
-              courseId
-              activityname
-              day
-              start
-              end
-              venue
-              course{
-                coursecode
-              }
-            }
-          }
-        activityId  
-        }
-      enrollments { 
-        items{
-          id
-          courseId
-          course{ 
-            id
-            coursecode
-            activity{
-              items{
-                courseId
-                activityname
-                coordinates
-                id
-                day
-                start
-                end
-                venue 
-                course{
-                  coursecode
-                }
-              }
-            }
-          }
-        }
-      }  
-    }
-}`
 
--- WebApp/src/Graphql/queries.js
+++ WebApp/src/Graphql/queries.js
@@ -1,422 +1,406 @@
-
-export const listLecturers=`query ListLecturers(
-  $filter: ModelLecturerFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listLecturers(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+export const getInstitution = /* GraphQL */ `
+  query GetInstitution($id: ID!) {
+    getInstitution(id: $id) {
       id
-      _version
-      _deleted
-      institutionId
-      institution{
-        logo
-        name
-      }
-      firstname
-      lastname
-      email
-      courses { 
-        items{ 
-          id 
-          lecturerId
-          coursecode
-          activity{ 
-            items{
-              id
-              activityname
-              day
-              start
-              end
-              venue
-              coordinates
-            }
-          }
-        }
-      }
-    }
-    nextToken
-  }
-}`
-
-export const getInstitution=`query GetInstitution($id: ID!) {
-  getInstitution(id: $id) {
-    id
-    name
-    lectureremails
-    domains
-    admin {
-      id
-      institutionId
-      firstname
-      lastname
-      email
-    }
-    adminId
-  }
-}
-`
-;
-export const listInstitutions=`query ListInstitutions(
-  $filter: ModelInstitutionFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listInstitutions(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-
-      id
-      logo
       name
-      domains
       lectureremails
-      admin { 
+      logo
+      domains
+      admin {
         id
+        institutionId
         firstname
         lastname
         email
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
       }
-      _deleted
-      _version
-    }
-    nextToken
-  }
-}`
-export const getAdmin=`query GetAdmin($id: ID!) {
-  getAdmin(id: $id) {
-    id
-    institutionId
-    firstname
-    lastname
-    email
-    institution {
-      id
-      name
-      logo
-      lectureremails
-      domains
       adminId
-    }
-  }
-}`;
-
-export const lecturersByInstitutionId=`query LecturersByInstitutionId(
-  $institutionId: ID!
-  $sortDirection: ModelSortDirection
-  $filter: ModelLecturerFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  lecturersByInstitutionId(
-    institutionId: $institutionId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
-  ) {
-    items {
-      id
-      institutionId
-      firstname
-      lastname
+      createdAt
+      updatedAt
       _version
-    _deleted
-      email
-      courses{
-        items{
-          id
-          coursecode
-        }
-      }
-    }
-    nextToken
-  }
-}
-`
-
-export const listCourses=`query ListCourses(
-  $filter: ModelCourseFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listCourses(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      institutionId
-      lecturerId
-      coursecode
-    }
-    nextToken
-  }
-}`
-
-export const listAdmins=`query ListAdmins(
-  $filter: ModelAdminFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listAdmins(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      institutionId
-      firstname
-      lastname
-      email
       _deleted
-      _version
-      institution{
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
+export const listInstitutions = /* GraphQL */ `
+  query ListInstitutions(
+    $filter: ModelInstitutionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listInstitutions(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
         id
         name
-        logo
-        domains
-        lectureremails
-        courses{
-          items{
-            id
-            coursecode
-          }
-        }
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
       }
-    }
-    nextToken
-  }
-}`
-
-export const listAnnouncements=`query ListAnnouncements(
-  $filter: ModelAnnouncementFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listAnnouncements(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      course{
-        coursecode
-      }
-      body
-      title
-      date
-      venue
-      title
-      type
-      course{
-        coursecode
-      }
-    }
-    nextToken
-  }
-}`
-
-export const getLecturer=`query GetLecturer($id: ID!) {
-  getLecturer(id: $id) {
-    id
-    institutionId
-    firstname
-    lastname
-    email
-    _version
-    _deleted
-    institution {
-      id
-      name
-      lectureremails
-      domains
-      adminId
-    }
-    courses {
       nextToken
+      startedAt
+      __typename
     }
   }
-}`
+`;
 
-export const searchLecturers=`query SearchLecturers(
-  $filter: SearchableLecturerFilterInput
-  $sort: [SearchableLecturerSortInput]
-  $limit: Int
-  $nextToken: String
-  $from: Int
-  $aggregates: [SearchableLecturerAggregationInput]
-) {
-  searchLecturers(
-    filter: $filter
-    sort: $sort
-    limit: $limit
-    nextToken: $nextToken
-    from: $from
-    aggregates: $aggregates
-  ) {
-    items {
+export const getAdmin = /* GraphQL */ `
+  query GetAdmin($id: ID!) {
+    getAdmin(id: $id) {
       id
       institutionId
       firstname
       lastname
+      userRole
       email
-      _version
-    _deleted
-      courses{
-        items{
-          id
-          coursecode
-        }
-      }
-    }
-    nextToken
-  }
-}
-`
-
-export const announcementsByDate=`query AnnouncementsByDate ( 
-        $year: String!,  
-        $createdAt: ModelStringKeyConditionInput, 
-        $sortDirection: ModelSortDirection, 
-        $filter: ModelAnnouncementFilterInput, 
-        $limit: Int, 
-        $nextToken: String
-      ){
-          announcementsByDate( 
-            year:$year,  
-            createdAt: $createdAt, 
-            sortDirection: $sortDirection, 
-            filter: $filter, 
-            limit: $limit, 
-            nextToken: $nextToken
-            ){ 
-              items{ 
-                id
-                title
-                body
-                date
-                createdAt
-                course{
-                  coursecode   
-                }
-                type
-                _version
-                _deleted
-              }
-              nextToken
-            }
-}`
-
-export const searchCourses=`query SearchCourses(
-  $filter: SearchableCourseFilterInput
-  $sort: [SearchableCourseSortInput]
-  $limit: Int
-  $nextToken: String
-  $from: Int
-  $aggregates: [SearchableCourseAggregationInput]
-) {
-  searchCourses(
-    filter: $filter
-    sort: $sort
-    limit: $limit
-    nextToken: $nextToken
-    from: $from
-    aggregates: $aggregates
-  ) {
-    items {
-      id
-      lecturerId
-      institutionId
-      coursecode
-      _deleted
-      _version
-    }
-    nextToken
-    aggregateItems {
-      name
-      result {
-        ... on SearchableAggregateScalarResult {
-          value
-        }
-        ... on SearchableAggregateBucketResult {
-          buckets {
-            key
-            doc_count
-          }
-        }
-      }
-    }
-  }
-}
-`
-export const searchLecturerByCourses=`query SearchCourses(
-  $filter: SearchableCourseFilterInput
-  $sort: [SearchableCourseSortInput]
-  $limit: Int
-  $nextToken: String
-  $from: Int
-  $aggregates: [SearchableCourseAggregationInput]
-) {
-  searchCourses(
-    filter: $filter
-    sort: $sort
-    limit: $limit
-    nextToken: $nextToken
-    from: $from
-    aggregates: $aggregates
-  ) {
-    items {
-      lecturer { 
+      owner
+      institution {
         id
+        name
+        lectureremails
+        logo
+        domains
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
+      }
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+  }
+`;
+export const listAdmins = /* GraphQL */ `
+  query ListAdmins(
+    $filter: ModelAdminFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listAdmins(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        institutionId
         firstname
         lastname
-        institutionId
-        _deleted
+        email
+        institution {
+          id
+          name
+          lectureremails
+          logo
+          domains
+          createdAt
+          updatedAt
+          _version
+          _deleted
+          _lastChangedAt
+          __typename
+        }
+        createdAt
+        updatedAt
         _version
-        courses{
-          items{
-            coursecode
-            id
-            lecturerId
-            institutionId
-          }
-        }
+        _deleted
+        _lastChangedAt
+        __typename
       }
-    }
-    nextToken
-    total
-    aggregateItems {
-      name
-      result {
-        ... on SearchableAggregateScalarResult {
-          value
-        }
-        ... on SearchableAggregateBucketResult {
-          buckets {
-            key
-            doc_count
-          }
-        }
-      }
+      nextToken
+      startedAt
+      __typename
     }
   }
-}
-`
+`;
+export const listLecturers = /* GraphQL */ `
+  query ListLecturers(
+    $filter: ModelLecturerFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listLecturers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        institutionId
+        firstname
+        lastname
+        email
+        institution {
+          id
+          name
+          logo
+          adminId
+          createdAt
+          updatedAt
+          _version
+          _deleted
+          _lastChangedAt
+          __typename
+        }
+        courses {
+          items {
+            id
+            coursecode
+            lecturerId
+            activity {
+              items{
+                id
+                activityname
+                day
+                start
+                end
+                venue 
+                coordinates
+                _version
+                _deleted
+                _lastChangedAt
+              }
+              nextToken
+              startedAt
+              __typename
+            }
+            createdAt
+            updatedAt
+            _version
+            _deleted
+            _lastChangedAt
+            __typename
+          }
+          nextToken
+          startedAt
+          __typename
+        }
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
+      }
+      nextToken
+      startedAt
+      __typename
+    }
+  }
+`;
 
-export const listAdminApplications=`
-             query listAdminApplications(
-                  $filter: ModelAdminApplicationFilterInput, 
-                  $limit: Int, 
-                  $nextToken: String)
-                  {
-                    listAdminApplications(
-                      filter: $filter
-                      limit :$limit
-                      nextToken : $nextToken
-                    ){
-                      items{
-                        id
-                        name
-                        firstname
-                        email
-                        status
-                        createdAt
-                        _version
-                        _deleted
-                      }
-                      nextToken
-                    }
-                  }`
-                
+export const lecturersByInstitutionId = /* GraphQL */ `
+  query LecturersByInstitutionId(
+    $institutionId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelLecturerFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    lecturersByInstitutionId(
+      institutionId: $institutionId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        institutionId
+        firstname
+        lastname
+        email
+        courses {
+          items {
+            id
+            institutionId
+            lecturerId
+            coursecode
+            createdAt
+            updatedAt
+            _version
+            _deleted
+            _lastChangedAt
+            __typename
+          }
+          nextToken
+          startedAt
+          __typename
+        }
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
+      }
+      nextToken
+      startedAt
+      __typename
+    }
+  }
+`;
+export const listCourses = /* GraphQL */ `
+  query ListCourses(
+    $filter: ModelCourseFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listCourses(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        institutionId
+        lecturerId
+        coursecode
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        owner
+        __typename
+      }
+      nextToken
+      startedAt
+      __typename
+    }
+  }
+`;
+export const coursesByInstitutionId = /* GraphQL */ `
+  query CoursesByInstitutionId(
+    $institutionId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelCourseFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    coursesByInstitutionId(
+      institutionId: $institutionId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        institutionId
+        lecturerId
+        coursecode
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        owner
+        __typename
+      }
+      nextToken
+      startedAt
+      __typename
+    }
+  }
+`;
+
+
+export const announcementsByDate = /* GraphQL */ `
+  query AnnouncementsByDate(
+    $year: String!
+    $createdAt: ModelStringKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelAnnouncementFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    announcementsByDate(
+      year: $year
+      createdAt: $createdAt
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        courseId
+        lecturerId
+        description
+        start
+        end
+        date
+        course {
+          coursecode
+          _version
+          _deleted
+          _lastChangedAt
+          __typename
+        }
+        body
+        title
+        type
+        year
+        createdAt
+        announcementStatus {
+          SMS
+          PUSH
+          EMAIL
+          announcement_Matrix {
+            SMS
+            PUSH
+            EMAIL
+            __typename
+          }
+          info
+          __typename
+        }
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
+      }
+      nextToken
+      startedAt
+      __typename
+    }
+  }
+`;
+
+export const listAdminApplications = /* GraphQL */ `
+  query ListAdminApplications(
+    $filter: ModelAdminApplicationFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listAdminApplications(
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        name
+        firstname
+        email
+        status
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
+      }
+      nextToken
+      startedAt
+      __typename
+    }
+  }
+`;
