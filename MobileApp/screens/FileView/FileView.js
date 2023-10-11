de4fd1a9d7f23d5657f41583c9236e5252a96d52
u20612894 <u20612894@tuks.co.za>
2023-10-11 14:30:48 +0200

    File List
    Using Datastore on File List

--- MobileApp/screens/FileView/FileView.js
+++ MobileApp/screens/FileView/FileView.js
@@ -13,9 +13,10 @@ import {
 import { Card } from "react-native-paper";
 import { Storage } from "aws-amplify";
 import { MaterialCommunityIcons } from "react-native-vector-icons";
-import { Auth, API } from "aws-amplify"
+import { Auth, API,DataStore } from "aws-amplify"
 import { getStudent } from "../../graphql/queries";
 import { useStudent } from "../../ContextProviders/StudentContext";
+import { Student,Institution } from "../../models";
 
 //graphQL call to get the university of the student, which will be used to get the file from that folder.
 //let studentUniversity = "UniversityOfPretoria";
@@ -61,29 +62,33 @@ const BucketFilesScreen = () => {
      let error = "There appear to be network issues.Please try again later"
      try {
        let stu=student;
-       if(student===null){
-         const user = await Auth.currentAuthenticatedUser()
-         let studentEmail = user.attributes.email; 
-         stu = await API.graphql({
-           query: getStudent,
-           variables: {id:user.attributes.sub}
-         })
+      //  if(student===null){
+      //    const user = await Auth.currentAuthenticatedUser()
+      //    let studentEmail = user.attributes.email; 
+      //    stu = await API.graphql({
+      //      query: getStudent,
+      //      variables: {id:user.attributes.sub}
+      //    })
          
-         stu=stu.data.getStudent;
-         if(stu===false || stu===undefined){
-           throw Error();
-         }
-         updateStudent(stu);
-       }
-     
-       sU = stu.institution.name
+      //    stu=stu.data.getStudent;
+      //    if(stu===false || stu===undefined){
+      //      throw Error();
+      //    }
+      //    updateStudent(stu);
+      //  }
+      const user = await Auth.currentAuthenticatedUser();
+      const id=user.attributes.sub;
+      stu = await DataStore.query(Student, id);
+       let sU = await stu.institution;
+       sU=sU.name;
        const words = sU.split(/\s+/); // Split the name into words
        sU = words
          .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Convert each word to camel case
          .join(""); // Join the words without spaces
-       await setStudentUniversity(sU)
+       setStudentUniversity(sU)
        return sU
      } catch (e) {
+      console.log(e);
        Alert.alert(error);
      }
    } 
