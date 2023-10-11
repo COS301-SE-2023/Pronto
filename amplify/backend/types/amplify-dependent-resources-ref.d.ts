ebab04f4cf4752da7246187bd7bf9175dfd871c3
Bandisa Masilela <89698022+Bandisa@users.noreply.github.com>
2023-10-11 12:53:40 +0200

    added analytics types

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "analytics": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
   "api": {
     "AdminQueries": {
       "ApiId": "string",
